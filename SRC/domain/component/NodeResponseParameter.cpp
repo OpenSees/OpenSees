ed4b902813c07df1d16cf8b0dac6634901ad155f
mhscott <Michael.Scott@oregonstate.edu>
2012-02-16 03:33:52 +0000

    Adding NodeResponseParameter

---
+++ SRC/domain/component/NodeResponseParameter.cpp
@@ -0,0 +1,126 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 2001, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+                                                                        
+// $Revision: 1.9 $
+// $Date: 2008-08-26 15:43:43 $
+// $Source: /usr/local/cvs/OpenSees/SRC/domain/component/NodeResponseParameter.cpp,v $
+
+#include <classTags.h>
+#include <NodeResponseParameter.h>
+#include <Node.h>
+
+NodeResponseParameter::NodeResponseParameter(int passedTag, Node *theNode, 
+					     NodeResponseType theType,
+					     int theDOF)
+  :Parameter(passedTag,1976), myNode(theNode), myType(theType), myDOF(theDOF), currentValue(0.0)
+{
+
+}
+
+
+NodeResponseParameter::~NodeResponseParameter()
+{
+
+}
+
+int
+NodeResponseParameter::update(int newValue)
+{
+  currentValue = newValue;
+
+  return 0;
+}
+
+int
+NodeResponseParameter::update(double newValue)
+{
+  currentValue = newValue;
+
+  return 0;
+}
+
+/*
+int
+NodeResponseParameter::activate(bool active)
+{
+  return 0;
+}
+*/
+
+double
+NodeResponseParameter::getValue(void)
+{
+  const Vector *u = myNode->getResponse(myType);
+
+  currentValue = (*u)(myDOF-1);
+
+  return currentValue;
+}
+
+void
+NodeResponseParameter::setValue(double newValue)
+{
+  currentValue = newValue;
+}
+
+bool
+NodeResponseParameter::isImplicit(void)
+{
+  return true;
+}
+
+double
+NodeResponseParameter::getSensitivity(int index)
+{
+  double dudh = myNode->getDispSensitivity(myDOF, index);
+  //opserr << "NRP " << this->getTag() << " index = " << index << " dudh = " << dudh << endln;
+  return dudh;
+}
+
+double
+NodeResponseParameter::getPerturbation(void)
+{
+  return 0.001*this->getValue();
+}
+
+void
+NodeResponseParameter::Print(OPS_Stream &s, int flag)  
+{
+  s << "NodeResponseParameter, tag = " << this->getTag() << endln;
+  myNode->Print(s, flag);
+}
+
+void
+NodeResponseParameter::setDomain(Domain *theDomain)
+{
+  return;
+}
+
+int 
+NodeResponseParameter::sendSelf(int commitTag, Channel &theChannel)
+{
+  return 0;
+}
+
+int 
+NodeResponseParameter::recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker &theBroker)
+{
+  return 0;
+}
---
+++ SRC/domain/component/NodeResponseParameter.h
@@ -0,0 +1,71 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 2001, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+                                                                        
+// $Revision: 1.7 $
+// $Date: 2008-08-26 15:38:37 $
+// $Source: /usr/local/cvs/OpenSees/SRC/domain/component/Parameter.h,v $
+
+#ifndef NodeResponseParameter_h
+#define NodeResponseParameter_h
+
+#include <Parameter.h>
+#include <string.h>
+
+class Node;
+class MovableObject;
+class Channel;
+class FEM_ObjectBroker;
+class Domain;
+
+class NodeResponseParameter : public Parameter
+{
+ public:
+  NodeResponseParameter(int tag, Node *theNode, NodeResponseType theType, int theDOF);
+  virtual ~NodeResponseParameter();
+  
+  virtual void Print(OPS_Stream &s, int flag =0);
+  
+  virtual int update(int newValue); 
+  virtual int update(double newValue); 
+  //virtual int activate(bool active);
+  virtual double getValue(void);
+  virtual void setValue(double newValue);
+
+  // To make it compile -- MHS 9/28/2011
+  virtual bool isImplicit(void);
+  virtual double getSensitivity(int index);
+  virtual double getPerturbation(void);
+  virtual const char *getType(void) {return "FEResponse";}
+
+  virtual void setDomain(Domain *theDomain);
+  virtual int sendSelf(int commitTag, Channel &theChannel);  
+  virtual int recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker &theBroker);
+
+ protected:
+  
+ private:
+  Node *myNode;
+  NodeResponseType myType;
+  int myDOF;
+  
+  double currentValue;
+};
+
+#endif
