cc80a7b0eb88ec6211dffd831cbb4fa66bd0949b
fmckenna <fmckenna@berkeley.edu>
2012-03-09 19:39:31 +0000

    fmk - changes for new convergence tests

--- SRC/classTags.h
+++ SRC/classTags.h
@@ -77,6 +77,8 @@
 #define CONVERGENCE_TEST_CTestRelativeEnergyIncr            6
 #define CONVERGENCE_TEST_CTestRelativeTotalNormDispIncr     7
 #define CONVERGENCE_TEST_CTestFixedNumIter                  8
+#define CONVERGENCE_TEST_NormDispAndUnbalance               9
+#define CONVERGENCE_TEST_NormDispOrUnbalance               10
 
 
 #define GRND_TAG_ElCentroGroundMotion                 1
