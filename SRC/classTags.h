9b53ad9eb64e095fc5f66cc4d26bca414eff1763
mhscott <Michael.Scott@oregonstate.edu>
2012-09-18 20:42:29 +0000

    change tag of PFEMLinSOE to tag of PFEMSolver

--- SRC/classTags.h
+++ SRC/classTags.h
@@ -745,7 +745,6 @@
 #define LinSOE_TAGS_MumpsSOE 23
 #define LinSOE_TAGS_MumpsParallelSOE 24
 #define LinSOE_TAGS_MPIDiagonalSOE 25
-#define LinSOE_TAGS_PFEMLinSOE    26
 
 
 #define SOLVER_TAGS_FullGenLinLapackSolver  	1
@@ -773,6 +772,7 @@
 #define SOLVER_TAGS_MumpsSolver			      	23
 #define SOLVER_TAGS_MumpsParallelSolver			24
 #define SOLVER_TAGS_MPIDiagonalSolver 25
+#define SOLVER_TAGS_PFEMSolver 26
 
 #define RECORDER_TAGS_ElementRecorder		1
 #define RECORDER_TAGS_NodeRecorder		2
