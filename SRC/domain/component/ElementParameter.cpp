991733408aa51f2cecfb263a075c1c233e1e6789
fmckenna <fmckenna@berkeley.edu>
2013-02-21 20:06:06 +0000

    fmk - initial import

---
+++ SRC/domain/component/ElementParameter.cpp
@@ -0,0 +1,250 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 2001, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+                                                                        
+// $Revision: 1.9 $
+// $Date: 2008-08-26 15:43:43 $
+// $Source: /usr/local/cvs/OpenSees/SRC/domain/component/ElementParameter.cpp,v $
+
+#include <classTags.h>
+#include <ElementParameter.h>
+#include <DomainComponent.h>
+#include <Domain.h>
+#include <Element.h>
+#include <Channel.h>
+#include <Message.h>
+
+ElementParameter::ElementParameter(int passedTag,
+				   int eleTag,
+				   const char **theArgv, int theArgc)
+  :Parameter(passedTag, PARAMETER_TAG_ElementParameter),
+   eleTags(1), numChannels(0), theChannels(0)
+{
+  argc = theArgc;
+  argv = 0;
+  argvSize = 0;
+  
+  if (argc != 0) {
+    argv = new char *[argc];
+    for (int i=0; i< argc; i++) {
+      int lengthArgvI = strlen(theArgv[i]);
+      argvSize += lengthArgvI + 1;
+    }
+
+    argv[0] = new char [argvSize];
+    strcpy(argv[0], theArgv[0]);
+    argvSize = strlen(theArgv[0]) + 1;
+
+    for (int i=1; i< argc; i++) {
+      int lengthArgvI = strlen(theArgv[i]);
+      argv[i] = argv[i-1]+lengthArgvI+1;
+      strcpy(argv[i], theArgv[i]);
+      argvSize += lengthArgvI + 1;
+    }
+  }
+
+  eleTags[0] = eleTag;
+}
+
+
+ElementParameter::ElementParameter()
+  :Parameter(-1, PARAMETER_TAG_ElementParameter),
+   numChannels(0), theChannels(0)
+{
+  argc = 0;
+  argv = 0;
+  argvSize = 0;
+}
+
+ElementParameter::~ElementParameter()
+{
+  if (argv != 0)
+    delete [] argv[0]; // stored in 1 array
+ 
+  delete [] argv;
+
+  if (theChannels != 0)
+    delete [] theChannels;
+}
+
+
+int 
+ElementParameter::update(int newValue)
+{
+  if (numChannels != 0) {
+    
+  }
+  
+  return this->Parameter::update(newValue);
+}
+
+int 
+ElementParameter::update(double newValue)
+{
+  if (numChannels != 0) {
+
+  }
+
+  return this->Parameter::update(newValue);
+}
+
+int
+ElementParameter::addComponent(int eleTag, const char **theArgv, int theArgc)
+{
+  int numEle = eleTags.Size();
+  eleTags[numEle] = eleTag;
+
+  if (theDomain != 0) {
+    Element *theEle = theDomain->getElement(eleTag);
+    if (theEle != 0)
+      return this->Parameter::addComponent(theEle, theArgv, theArgc);
+  }
+
+  if (argc != theArgc) {
+    opserr << "ElementParameter::addComponent(int eleTag) " << eleTag << " argc passed differ from stored, won't work in SP\n";
+    return 0;
+  }
+  for (int i=0; i<argc; i++) {
+    if (strcmp(argv[i], theArgv[i]) != 0) {
+      opserr << "ElementParameter::addComponent(int eleTag) " << eleTag << " argc passed differ from stored, won't work in SP\n";
+    }
+  }
+  return 0;
+}
+
+void
+ElementParameter::setDomain(Domain *aDomain)
+{
+  theDomain = aDomain;
+  this->Parameter::clean();
+
+  const char **theArgv = (const char **)argv;
+  int numEle = eleTags.Size();
+  for (int i=0; i<numEle; i++) {
+    int eleTag = eleTags[i];
+    Element *theEle = theDomain->getElement(eleTag);
+    if (theEle != 0)
+      this->Parameter::addComponent(theEle, theArgv, argc);
+  }
+}
+
+int 
+ElementParameter::sendSelf(int commitTag, Channel &theChannel)
+{
+  ID idData(4);
+  idData(0) = this->getTag();
+  idData(1) = eleTags.Size();
+  idData(3) = argc;
+  idData(2) = argvSize;
+  
+  if (theChannel.sendID(0, commitTag, idData) < 0) {
+
+  }
+
+  if (theChannel.sendID(0, commitTag, eleTags) < 0) {
+
+  }
+
+  ID argData(argc-1);
+
+  for (int i=0; i<argc-1; i++) {
+    argData(i) = argv[i+1]-argv[i];
+  }
+
+  if (theChannel.sendID(0, commitTag, argData) < 0) {
+
+  }
+
+  Message msgData(argv[0], argvSize);
+  if (theChannel.sendMsg(0, commitTag, msgData) < 0) {
+
+  }
+
+
+  Channel **newChannels = new Channel *[numChannels+1];
+  for (int i=0; i<numChannels; i++)
+    newChannels[i] = theChannels[i];
+  
+  newChannels[numChannels] = &theChannel;
+  numChannels++;
+  
+  if (theChannels != 0)
+    delete [] theChannels;
+
+  return 0;
+}
+
+int 
+ElementParameter::recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker &theBroker)
+{
+  ID idData(4);
+
+  if (theChannel.recvID(0, commitTag, idData) < 0) {
+
+  }
+
+  this->setTag(idData(0));
+
+  eleTags.resize(idData(1));
+
+  if (theChannel.recvID(0, commitTag, idData) < 0) {
+
+  }
+
+  if (theChannel.recvID(0, commitTag, eleTags) < 0) {
+
+  }
+
+  if (argv != 0) {
+    if (argv[0] != 0)
+      delete [] argv[0];
+
+    delete [] argv;
+  }
+
+  argc = idData(3);
+  argvSize = idData(2);
+
+  ID argData(argc-1);
+  if (theChannel.recvID(0, commitTag, argData) < 0) {
+
+  }
+
+  argv = new char *[argc];
+  argv[0] = new char[argvSize];
+
+  for (int i=0; i<argc-1; i++)
+    argv[i+1] = argv[0] + argData[i];
+
+  Message msgData(argv[0], argvSize);
+  if (theChannel.recvMsg(0, commitTag, msgData) < 0) {
+
+  }  
+
+  if (theChannels != 0)
+    delete [] theChannels;
+
+  theChannels = new Channel *[1];
+  theChannels[0] = &theChannel;
+  numChannels = -1;
+
+
+  return 0;
+}
+
---
+++ SRC/domain/component/ElementParameter.h
@@ -0,0 +1,64 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 2001, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+                                                                        
+// $Revision: 1.7 $
+// $Date: 2008-08-26 15:38:37 $
+// $Source: /usr/local/cvs/OpenSees/SRC/domain/component/Parameter.h,v $
+
+#ifndef ElementParameter_h
+#define ElementParameter_h
+
+#include <Parameter.h>
+#include <ID.h>
+
+class ElementParameter : public Parameter
+{
+ public:
+  ElementParameter(int tag, 
+		   int eleTag,
+		   const char **argv, 
+		   int argc);
+  ElementParameter();
+  ~ElementParameter();
+
+  int update(int newValue); 
+  int update(double newValue); 
+  
+  int addComponent(int, const char **argv, int argc);  
+
+  void setDomain(Domain *theDomain);
+
+  int sendSelf(int commitTag, Channel &theChannel);  
+  int recvSelf(int commitTag, Channel &theChannel, FEM_ObjectBroker &theBroker);
+
+ protected:
+  
+ private:
+  Domain *theDomain;
+  ID eleTags; // for each ele a tuple: eleTags, args index;
+  char **argv;
+  int argc;
+  int argvSize;
+
+  int numChannels;
+  Channel **theChannels;
+};
+
+#endif
