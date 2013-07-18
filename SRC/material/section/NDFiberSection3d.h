40dbcd30c199c9074d25893c12c196702230b750
mhscott <Michael.Scott@oregonstate.edu>
2013-07-18 22:51:46 +0000

    removing eCommit

--- SRC/material/section/NDFiberSection3d.h
+++ SRC/material/section/NDFiberSection3d.h
@@ -109,7 +109,6 @@ class NDFiberSection3d : public SectionForceDeformation
     static ID code;
 
     Vector e;          // trial section deformations 
-    Vector eCommit;    // committed section deformations 
     Vector *s;         // section resisting forces  (axial force, bending moment)
     Matrix *ks;        // section stiffness
 
