cfc955ed3fa5be486dc2cc0791de4d64e40ca096
fmckenna <fmckenna@berkeley.edu>
2013-10-03 22:24:38 +0000

    fmk - adding simple eigen verification example

--- EXAMPLES/verification/AISC25.tcl
+++ EXAMPLES/verification/AISC25.tcl
@@ -1,3 +1,4 @@
+
 #  PDelta Effects 
 
 # REFERENCES:
@@ -21,6 +22,10 @@ puts "------+--------+--------+---------+------+---------+--------+------"
 set formatString {%5s|%8s|%8s|%9s|%6s|%9s|%8s|%6s}
 puts [format $formatString numEle alpha Exact OpenSees %Error Exact OpenSees %Error]
 puts "------+--------+--------+---------+------+---------+--------+------"
+
+foreach {gTransf} {"Linear" "PDelta" "Corotational"} {
+    puts "$gTransf"
+
 foreach numEle {1 2 4 10} {
     foreach alpha {0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.67} {
 	wipe
@@ -39,11 +44,89 @@ foreach numEle {1 2 4 10} {
 	    node [expr $i +1] 0. [expr $i * $dY]
 	}
 	
-	geomTransf PDelta 1
+#	geomTransf PDelta 1
+#	geomTransf Corotational 1
+	geomTransf $gTransf 1
+	section Elastic 1 $E $A $I
 	set eleTag 1; set iNode 1; set jNode 2;
 	for {set i 0} {$i < $numEle} {incr i 1} {
 	    element elasticBeamColumn $eleTag $iNode $jNode $A $E $I 1
-	incr eleTag; incr iNode; incr jNode;
+#	    element forceBeamColumnCBDI $eleTag $iNode $jNode 1 "Legendre 1 4"
+	    incr eleTag; incr iNode; incr jNode;
+	}
+	
+	fix 1 1 1 1
+	
+	set u    [expr $PI/2.0*sqrt($alpha * $Pr/$Pel)]
+	
+	if {$u != 0} {
+	    set resU [expr ($H*$L*$L*$L/(3.0*$E*$I)) * (3.0*(tan(2.0*$u)-2.0*$u)/(8.0*$u*$u*$u))]
+	    set resM [expr $H*$L*(tan(2.*$u)/(2.*$u))]
+	} else {
+	    set resU [expr ($H*$L*$L*$L/(3.0*$E*$I))]
+	    set resM [expr $H*$L]
+	}
+	
+	timeSeries Linear 1
+	pattern Plain 1 1 {
+	    load  [expr $numEle+1] $H [expr -$alpha*$Pr] 0.
+	}
+	
+	constraints Plain
+	system ProfileSPD
+	numberer Plain
+	integrator LoadControl 1
+	test NormDispIncr 1.0e-12 6 0
+	algorithm Newton
+	analysis Static
+	analyze 1
+	
+	set delta [nodeDisp [expr $numEle + 1] 1]
+	set moment [lindex [eleResponse 1 forces] 2]
+	set formatString {%6.0f|%8.2f|%8.4f|%9.4f|%6.1f|%9.2f|%8.2f|%6.1f}
+	puts [format $formatString $numEle $alpha $resU $delta [expr 100*($resU-$delta)/$delta] $resM $moment [expr 100*($resM-$moment)/$moment] ]
+    }
+    puts "------+--------+--------+---------+------+---------+--------+------"
+}
+}
+# test on last one
+if {[expr abs(100*($resU-$delta)/$delta)] > 0.5 || [expr abs(100*($resM-$moment)/$moment)] > 0.5} {
+    set ok 1
+    puts "[expr abs(100*($resU-$delta)/$delta)] > 0.5 || [expr abs(100*($resM-$moment)/$moment)] > 0.5"
+}
+
+puts "Prismatic Beam Benchmark Problems\n"
+puts "    - Case 1 (Single Curcature) - forceBeamColumnCBDI element"
+puts "------+--------+-------------------------+-------------------------"
+puts "      |        |     Tip Displacement    |      Base Moment        "
+puts "------+--------+--------+---------+------+---------+--------+------"
+set formatString {%5s|%8s|%8s|%9s|%6s|%9s|%8s|%6s}
+puts [format $formatString numEle alpha Exact OpenSees %Error Exact OpenSees %Error]
+puts "------+--------+--------+---------+------+---------+--------+------"
+foreach numEle {1 } {
+    foreach alpha {0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.67} {
+	wipe
+	model Basic -ndm 2
+	
+	set E 29500.0
+	set A 51.7
+	set I 2150.0
+	
+	set Pel [expr $PI*$PI*$E*$I/($L*$L)]
+	set Pcr [expr $Pel/4.0]
+	set Pr  $Pcr
+	
+	set dY [expr $L/$numEle]
+	for {set i 0} {$i <= $numEle} {incr i 1} {
+	    node [expr $i +1] 0. [expr $i * $dY]
+	}
+	
+	geomTransf PDelta 1
+	section Elastic 1 $E $A $I
+	set eleTag 1; set iNode 1; set jNode 2;
+	for {set i 0} {$i < $numEle} {incr i 1} {
+	    element forceBeamColumnCBDI $eleTag $iNode $jNode 1 "Legendre 1 4"
+	    incr eleTag; incr iNode; incr jNode;
 	}
 	
 	fix 1 1 1 1
@@ -80,12 +163,7 @@ foreach numEle {1 2 4 10} {
     puts "------+--------+--------+---------+------+---------+--------+------"
 }
 
-# test on last one
-if {[expr abs(100*($resU-$delta)/$delta)] > 0.5 || [expr abs(100*($resM-$moment)/$moment)] > 0.5} {
-    set ok 1
-    puts "[expr abs(100*($resU-$delta)/$delta)] > 0.5 || [expr abs(100*($resM-$moment)/$moment)] > 0.5"
-}
-
+quit
 
 puts "\n\n    - Case 2 (Double Curvature)"
 puts "------+--------+-------------------------+-------------------------"
@@ -172,3 +250,71 @@ if {$ok == 0} {
 }
 close $results
 
+#https://wiki.csiamerica.com/display/tp/P-Delta+effect+for+a+cantilevered+column
+
+# P-Delta effect for cantilevered column
+# Calculation and Verification for P-Delta effects of a cantilevered column
+# SAP20000 Model ID: 109
+
+
+set E  30.0e9
+set L  10.0
+set A [expr 0.1*0.1]
+set I [expr 0.1*0.1*0.1*0.1/12.0]
+set H 45
+set P 4000
+
+set PI [expr 2.0*asin(1.0)]
+
+set ok 0
+set results []
+set counter 0
+
+puts "SAP2000 P-Delta Column Verification, Model ID 109\n"
+foreach numEle {1} {
+    wipe
+    model Basic -ndm 2
+	
+    set dY [expr $L/$numEle]
+    for {set i 0} {$i <= $numEle} {incr i 1} {
+	node [expr $i +1] 0. [expr $i * $dY]
+    }
+	
+    geomTransf PDelta 1
+    set eleTag 1; set iNode 1; set jNode 2;
+    for {set i 0} {$i < $numEle} {incr i 1} {
+	element elasticBeamColumn $eleTag $iNode $jNode $A $E $I 1
+	incr eleTag; incr iNode; incr jNode;
+    }
+	
+    fix 1 1 1 1
+    
+    if {$u != 0} {
+	set resU [expr ($H*$L*$L*$L/(3.0*$E*$I)) * (3.0*(tan(2.0*$u)-2.0*$u)/(8.0*$u*$u*$u))]
+	set resM [expr $H*$L*(tan(2.*$u)/(2.*$u))]
+    } else {
+	set resU [expr ($H*$L*$L*$L/(3.0*$E*$I))]
+	set resM [expr $H*$L]
+    }
+    
+    timeSeries Linear 1
+    pattern Plain 1 1 {
+	load  [expr $numEle+1] $H -$P 0.
+    }
+    
+    constraints Plain
+    system BandGEN
+    numberer Plain
+    integrator LoadControl 1
+    test NormDispIncr 1.0e-12 6 0
+    algorithm Newton
+    analysis Static
+    analyze 1
+    
+    set delta [nodeDisp [expr $numEle + 1] 1]
+    set moment [lindex [eleResponse 1 forces] 2]
+    set formatString {%6.0f|%8.2f|%8.4f|%9.4f|%6.1f|%9.2f|%8.2f|%6.1f}
+    puts [format $formatString $numEle $alpha $resU $delta [expr 100*($resU-$delta)/$delta] $resM $moment [expr 100*($resM-$moment)/$moment] ]
+}
+puts "------+--------+--------+---------+------+---------+--------+------"
+}
---
+++ EXAMPLES/verification/SmallEigen.tcl
@@ -0,0 +1,82 @@
+
+# Small 3dof eigen example
+#
+# 3 springs in series, K = 30,20,10, M = 11.1,22.2,33.3
+#
+# results as computed by matlab: 0.139300647653736 1.05865310768512 4.9582024008173
+
+# using following 3 commands:
+# format longG
+# K=[[50 -20 0];[-20 30 -10];[0 -10 10]]
+# M=[[11.1 0 0];[0 22.2 0];[0 0 33.3]]
+# eig(K,M)
+
+puts "SmallEigen.tcl: Small 3 dof 2 spring model: checking eigen results of smallest and larget values"
+puts "         results compared for eaxct against Matlab\n"
+
+set exactResults {0.139300647653736 1.05865310768512  4.9582024008173}
+
+wipe
+model Basic -ndm 1 -ndf 1
+node 1 0 
+node 2 0 -mass 11.1
+node 3 0 -mass 22.2
+node 4 0 -mass 33.3
+uniaxialMaterial Elastic 1 30
+uniaxialMaterial Elastic 2 20
+uniaxialMaterial Elastic 3 10
+fix 1 1
+element zeroLength 1 1 2 -mat 1 -dir 1
+element zeroLength 2 2 3 -mat 2 -dir 1
+element zeroLength 3 3 4 -mat 3 -dir 1
+
+
+
+puts "Checking Computation of Smallest Eigenvalues:"
+set testOK 0
+
+set tol 1.0e-6
+set numEigen 2
+set eigenValues [eigen $numEigen]
+
+set formatString {%15s%15s}
+puts [format $formatString OpenSees Exact]
+set formatString {%15.6f%15.6f}
+for {set i 0} {$i<$numEigen} {incr i 1} {
+    set OpenSeesR [lindex $eigenValues  $i]
+    set exactR    [lindex $exactResults $i]
+    puts [format $formatString $OpenSeesR  $exactR]
+
+    if {[expr abs($OpenSeesR-$exactR)] > $tol} {
+	set testOK -1;
+	puts "failed-> [expr abs($OpenSeesR-$exactR)] $tol"
+    }
+}
+
+puts "\nChecking Computation of Largest Eigenvalues:"
+puts [eigen -findLargest $numEigen]
+set eigenValues [eigen -findLargest $numEigen]
+
+set formatString {%15s%15s}
+puts [format $formatString OpenSees Exact]
+set formatString {%15.6f%15.6f}
+for {set i 0} {$i<$numEigen} {incr i 1} {
+    set OpenSeesR [lindex $eigenValues  $i]
+    set exactR    [lindex $exactResults [expr $i+1]]
+    puts [format $formatString $OpenSeesR  $exactR]
+
+    if {[expr abs($OpenSeesR-$exactR)] > $tol} {
+	set testOK -1;
+	puts "failed-> [expr abs($OpenSeesR-$exactR)] $tol"
+    }
+}
+
+set results [open results.out a+]
+if {$testOK == 0} {
+    puts "PASSED Verification Test SmallEigen.tcl \n\n"
+    puts $results "PASSED : SmallEigen.tcl"
+} else {
+    puts "FAILED Verification Test SmallEigen.tcl \n\n"
+    puts $results "FAILED : SmallEigen.tcl"
+}
+close $results
