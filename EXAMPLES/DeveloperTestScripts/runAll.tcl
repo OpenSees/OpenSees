a10782c2d88b9e3b091ed7d2ff131e77609b33cd
fmckenna <fmckenna@berkeley.edu>
2013-10-04 22:24:45 +0000

    fmk - adding to runAll

--- EXAMPLES/DeveloperTestScripts/runAll.tcl
+++ EXAMPLES/DeveloperTestScripts/runAll.tcl
@@ -3,3 +3,15 @@ wipe; source BiaxialConcrete_matTest.tcl
 
 puts "\n source ZeroLengthImpact3D_test.tcl"
 wipe; source ZeroLengthImpact3D_test.tcl 
+
+puts "\n source Cap.tcl"
+wipe; source Cap.tcl
+
+puts "\n source SteelBRB.tcl"
+wipe; source SteelBRB.tcl
+
+puts "\n source SimplifiedJ2.tcl"
+wipe; source SimplifiedJ2.tcl
+
+puts "\n source PlaneStressSimplifiedJ2.tcl"
+wipe; source PlaneStressSimplifiedJ2.tcl
