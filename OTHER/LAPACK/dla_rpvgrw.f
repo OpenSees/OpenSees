6942f05c34bb61e0af8f33221cf59650e7a681e3
fmckenna <fmckenna@berkeley.edu>
2013-10-24 00:00:20 +0000

    fmk - updating and adding routines for dgesvxx, a lapack solver that includes iterative refinement for use with porrly conditioned matrices

---
+++ OTHER/LAPACK/dla_rpvgrw.f
@@ -0,0 +1,80 @@
+      DOUBLE PRECISION FUNCTION DLA_RPVGRW( N, NCOLS, A, LDA, AF, LDAF )
+*
+*     -- LAPACK routine (version 3.2.2)                                 --
+*     -- Contributed by James Demmel, Deaglan Halligan, Yozo Hida and --
+*     -- Jason Riedy of Univ. of California Berkeley.                 --
+*     -- June 2010                                                    --
+*
+*     -- LAPACK is a software package provided by Univ. of Tennessee, --
+*     -- Univ. of California Berkeley and NAG Ltd.                    --
+*
+      IMPLICIT NONE
+*     ..
+*     .. Scalar Arguments ..
+      INTEGER            N, NCOLS, LDA, LDAF
+*     ..
+*     .. Array Arguments ..
+      DOUBLE PRECISION   A( LDA, * ), AF( LDAF, * )
+*     ..
+*
+*  Purpose
+*  =======
+* 
+*  DLA_RPVGRW computes the reciprocal pivot growth factor
+*  norm(A)/norm(U). The "max absolute element" norm is used. If this is
+*  much less than 1, the stability of the LU factorization of the
+*  (equilibrated) matrix A could be poor. This also means that the
+*  solution X, estimated condition numbers, and error bounds could be
+*  unreliable.
+*
+*  Arguments
+*  =========
+*
+*     N       (input) INTEGER
+*     The number of linear equations, i.e., the order of the
+*     matrix A.  N >= 0.
+*
+*     NCOLS   (input) INTEGER
+*     The number of columns of the matrix A. NCOLS >= 0.
+*
+*     A       (input) DOUBLE PRECISION array, dimension (LDA,N)
+*     On entry, the N-by-N matrix A.
+*
+*     LDA     (input) INTEGER
+*     The leading dimension of the array A.  LDA >= max(1,N).
+*
+*     AF      (input) DOUBLE PRECISION array, dimension (LDAF,N)
+*     The factors L and U from the factorization
+*     A = P*L*U as computed by DGETRF.
+*
+*     LDAF    (input) INTEGER
+*     The leading dimension of the array AF.  LDAF >= max(1,N).
+*
+*  =====================================================================
+*
+*     .. Local Scalars ..
+      INTEGER            I, J
+      DOUBLE PRECISION   AMAX, UMAX, RPVGRW
+*     ..
+*     .. Intrinsic Functions ..
+      INTRINSIC          ABS, MAX, MIN
+*     ..
+*     .. Executable Statements ..
+*
+      RPVGRW = 1.0D+0
+
+      DO J = 1, NCOLS
+         AMAX = 0.0D+0
+         UMAX = 0.0D+0
+         DO I = 1, N
+            AMAX = MAX( ABS( A( I, J ) ), AMAX )
+         END DO
+         DO I = 1, J
+            UMAX = MAX( ABS( AF( I, J ) ), UMAX )
+         END DO
+         IF ( UMAX /= 0.0D+0 ) THEN
+            RPVGRW = MIN( AMAX / UMAX, RPVGRW )
+         END IF
+      END DO
+      DLA_RPVGRW = RPVGRW
+      END
