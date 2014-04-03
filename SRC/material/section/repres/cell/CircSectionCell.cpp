330b99d9af098a4c64216c2592d29ca8fb212b9c
fmckenna <fmckenna@berkeley.edu>
2014-04-03 22:57:09 +0000

    fm k- initila import

---
+++ SRC/material/section/repres/cell/CircSectionCell.cpp
@@ -0,0 +1,80 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 1999, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+                                                                        
+// Written: fmk
+// March 2014
+
+#include <Matrix.h>
+#include <Vector.h>
+#include <Math.h>
+
+#include <CircSectionCell.h>
+
+CircSectionCell::CircSectionCell(double R1, double R2, double Alpha, double Theta)
+  :r1(R1), r2(R2), alpha(Alpha), theta(Theta), A(0.0), Centroid(2)
+{
+  double a = alpha/2.0;
+
+  double At = a*r2*r2;
+  double ct = 2.0*r2/(3.0*a);
+  double A1 = a*r1*r1;
+  double c1 = 2.0*r1/(3.0*a);
+  
+  A = At - A1;
+  double c = (At*ct - A1*c1)/A;
+
+  Centroid(0) = cos(Theta)/c;
+  Centroid(1) = sin(Theta)/c;
+  
+}
+
+
+CircSectionCell::~CircSectionCell()
+{
+
+}
+
+double CircSectionCell::getArea (void) const
+{
+  return A;
+}
+
+
+const Vector & 
+CircSectionCell::getCentroidPosition(void)
+{
+   return Centroid;
+}
+
+
+
+void CircSectionCell::Print(OPS_Stream &s, int flag) const
+{
+   s << "\nCell Type: CircSectionCell";
+   s << "\n\tr1: " << r1 << " r2: " << r2 << " alpha: " << alpha << " theta: " << theta << endln;
+}
+
+
+OPS_Stream &operator<<(OPS_Stream &s, const CircSectionCell &cell)
+{
+   cell.Print(s);
+   return s;
+}    
+
