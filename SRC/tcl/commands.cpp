111ad12aa6e0b1aaa3cf3b5eadb72529c80cbaee
fmckenna <fmckenna@berkeley.edu>
2014-04-15 16:17:12 +0000

    L.Xie and X.Lu, Tsinghua Univeristy - update for CuSP solver

--- SRC/tcl/commands.cpp
+++ SRC/tcl/commands.cpp
@@ -252,6 +252,10 @@ extern TransientIntegrator *OPS_NewGeneralizedAlpha(void);
 #include <SuperLU.h>
 #endif
 
+#ifdef _WIN32
+#include <CuSPSolver.h>
+#endif
+
 #ifdef _CULAS4
 #include <CulaSparseSolverS4.h>
 #endif
@@ -2734,6 +2738,65 @@ specifySOE(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char **argv)
       }
 #endif
   }
+
+#ifdef _WIN32
+  else if ((_stricmp(argv[1],"CuSP")==0)) {
+
+    
+    double relTol=1e-6;
+    int maxInteration=100000;
+    int preCond=1;			//diagonal
+    int solver=0;				//Bicg
+    int count = 2;
+    
+    
+    while (count < argc) {
+      
+      if (_stricmp(argv[count],"-rTol") == 0) {
+	count++;
+	if (count < argc)
+	  if (Tcl_GetDouble(interp, argv[count], &relTol) != TCL_OK)
+	    return TCL_ERROR;		     
+      }
+      else if ((_stricmp(argv[count],"-mInt") == 0) ) {
+	count++;
+	if (count < argc)
+	  if (Tcl_GetInt(interp, argv[count], &maxInteration) != TCL_OK)
+	    return TCL_ERROR;		     
+      }
+      else if ((_stricmp(argv[count],"-pre") == 0) ) {
+	count++;
+	if (count < argc)
+	  if ((_stricmp(argv[count],"none") == 0))
+	    preCond=0;
+	  else if ((_stricmp(argv[count],"diagonal") == 0))
+	    preCond=1;
+	  else if ((_stricmp(argv[count],"ainv") == 0))
+	    preCond=2;
+	  else
+	    return TCL_ERROR;
+      } else if ((_stricmp(argv[count],"-solver") == 0)) {
+	count++;
+	if (count < argc)
+	  if ((_stricmp(argv[count],"bicg") == 0))
+	    solver=0;
+	  else if ((_stricmp(argv[count],"bicgstab") == 0))
+	    solver=1;
+	  else if ((_stricmp(argv[count],"cg") == 0))
+	    solver=2;
+	  else if ((_stricmp(argv[count],"gmres") == 0))
+	    solver=3;
+	  else
+	    return TCL_ERROR;
+      }
+      count++;
+    }
+    
+    CuSPSolver* theSolver = new CuSPSolver(maxInteration,relTol,preCond,solver);
+    theSOE = new SparseGenRowLinSOE(* theSolver);
+    
+  }
+#endif
 
 #if defined(_CULAS4) || defined(_CULAS5)
   // CULA SPARSE
