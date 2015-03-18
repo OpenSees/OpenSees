35d50257228f9af47c4e3529a145baddec911a7e
fmckenna <fmckenna@berkeley.edu>
2015-03-18 23:51:39 +0000

    fmk - fixes for a decent compiler

--- SRC/material/nD/UWmaterials/ManzariDafaliasRO.h
+++ SRC/material/nD/UWmaterials/ManzariDafaliasRO.h
@@ -62,7 +62,7 @@ class ManzariDafaliasRO : public ManzariDafalias
  
     NDMaterial *getCopy(const char *type);
 
-	void ManzariDafaliasRO::integrate();
+   void integrate();
     int commitState(void);
 
     NDMaterial *getCopy(void);
