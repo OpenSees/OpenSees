a197ea9feb8b53f7bd5088effd0ccf06d2579fa4
fmckenna <fmckenna@berkeley.edu>
2015-03-31 00:38:12 +0000

    S.Kechidi - code improvements

--- SRC/material/uniaxial/CubicSpline.h
+++ SRC/material/uniaxial/CubicSpline.h
@@ -1,42 +1,39 @@
-#ifndef CubicSpline_h
-#define CubicSpline_h
-
+#pragma once
 class CubicSpline
 {
 		// N-1 spline coefficients for N points
 	
 public:
-  /*int xOrigLength;
-    int yOrigLength;
-    int _lastIndex;
-    double* a;
-    double* b;
-    double* xOrig;
-    double* yOrig;*/
-  
-  
-  double* xs,* ys;
-  int xsL,ysL;
-  double* c1s,* c2s,* c3s;
-  int c1sc,c2sc,c3sc,c1sL,c2sL,c3sL;
-  int sc1sL,sc2sL,sc3sL;
-  CubicSpline(void);
-  ~CubicSpline(void);
-  
-  void Fit(double* x, int xl, double* y, int yl);
-  double Eval(double x);
-  double EvalT(double x);
-  
-  /*double* FitAndEval(double* x, int xLength, double* y,int yLength, double* xs, int xsLength);
-    void Fit(double* x, int xLength, double* y,int yLength);
-    int GetNextXIndex(double x);
-    double* Eval(double* x, int xLength);
-    void Fit(double* x,int xLength, double* y,int yLength, double a0, double b0, double an, double bn);
-    void Fit3(double* x,int xLength, double* y,int yLength, double a0, double b0, double an, double bn);
-    void Fit4(double* x,int xLength, double* y,int yLength, double a0, double b0, double an, double bn);
-    double Eval(double x);
-    double EvalT(double x);
-    void Affiche();*/
+	/*int xOrigLength;
+	int yOrigLength;
+	int _lastIndex;
+	double* a;
+	double* b;
+	double* xOrig;
+	double* yOrig;*/
+
+
+    double* xs,* ys;
+	int xsL,ysL;
+    double* c1s,* c2s,* c3s;
+	int c1sc,c2sc,c3sc,c1sL,c2sL,c3sL;
+	int sc1sL,sc2sL,sc3sL;
+	CubicSpline(void);
+	~CubicSpline(void);
+
+	void Fit(double* x, int xl, double* y, int yl);
+	double Eval(double x);
+	double EvalT(double x);
+
+	/*double* FitAndEval(double* x, int xLength, double* y,int yLength, double* xs, int xsLength);
+	void Fit(double* x, int xLength, double* y,int yLength);
+	int GetNextXIndex(double x);
+	double* Eval(double* x, int xLength);
+	void Fit(double* x,int xLength, double* y,int yLength, double a0, double b0, double an, double bn);
+	void Fit3(double* x,int xLength, double* y,int yLength, double a0, double b0, double an, double bn);
+	void Fit4(double* x,int xLength, double* y,int yLength, double a0, double b0, double an, double bn);
+	double Eval(double x);
+	double EvalT(double x);
+	void Affiche();*/
 };
 
-#endif
--- SRC/material/uniaxial/TriMatrix.h
+++ SRC/material/uniaxial/TriMatrix.h
@@ -1,36 +1,35 @@
-#ifndef TriDiagonalMatrixf_h
-#define TriDiagonalMatrixf_h
-
+#ifndef _OPS_TriDiagonalMatrixF
+#define _OPS_TriDiagonalMatrixF
 class TriDiagonalMatrixF
 {		
- public:
-  /// <summary>
-  /// Construct an NxN matrix.
-  /// </summary>
-  TriDiagonalMatrixF(int n);
-  void SetMat(int row, int col, double value);
-  double GetMat(int row, int col);
-  double* Solve(double* d, int dLength);
+	public:
+		int Lenght;
+		/// <summary>
+		/// The values for the sub-diagonal. A[0] is never used.
+		/// </summary>
+		double *A;
 
-  ~TriDiagonalMatrixF(void);
+		/// <summary>
+		/// The values for the main diagonal.
+		/// </summary>
+		double *B;
 
-  int Lenght;
-  /// <summary>
-  /// The values for the sub-diagonal. A[0] is never used.
-  /// </summary>
-  double *A;
-  
-  /// <summary>
-  /// The values for the main diagonal.
-  /// </summary>
-  double *B;
-  
-  /// <summary>
-  /// The values for the super-diagonal. C[C.Length-1] is never used.
-  /// </summary>
-  double *C;
-  
-  int N();
+		/// <summary>
+		/// The values for the super-diagonal. C[C.Length-1] is never used.
+		/// </summary>
+		double *C;
+
+		int N();
+
+		~TriDiagonalMatrixF(void);
+		
+		/// <summary>
+		/// Construct an NxN matrix.
+		/// </summary>
+		TriDiagonalMatrixF(int n);
+		void SetMat(int row, int col, double value);
+		double GetMat(int row, int col);
+		double* Solve(double* d, int dLength);
 };
-
 #endif
+
