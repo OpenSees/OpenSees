0220f1b6fa2683880a927e5e02e0c751d2af1c4a
fmckenna <fmckenna@berkeley.edu>
2015-08-24 23:55:34 +0000

    fmk - moving from if,else to stl map

--- SRC/interpreter/OpenSeesUniaxialMaterialCommands.cpp
+++ SRC/interpreter/OpenSeesUniaxialMaterialCommands.cpp
@@ -1,15 +1,55 @@
-
 #include <UniaxialMaterial.h>
 #include <string.h>
+#include <map>
+
+
+struct char_cmp { 
+  bool operator () (const char *a,const char *b) const 
+  {
+    return strcmp(a,b)<0;
+  } 
+};
+
+typedef std::map<const char *, void *(*)(void), char_cmp> OPS_ParsingFunctionMap;
+
+static OPS_ParsingFunctionMap uniaxialMaterialsMap;
+static bool initDone = false;
 
 extern void *OPS_ElasticMaterial(void);
 extern void *OPS_ElasticPPMaterial(void);
 extern void *OPS_ParallelMaterial(void);
 
+int OPS_SetUpUniaxialMaterials(void) {
+  uniaxialMaterialsMap.insert(std::make_pair("Elastic", &OPS_ElasticMaterial));
+  uniaxialMaterialsMap.insert(std::make_pair("ElasticPP", &OPS_ElasticPPMaterial));
+  uniaxialMaterialsMap.insert(std::make_pair("Parallel", &OPS_ParallelMaterial));
+
+  return 0;
+}
+
 UniaxialMaterial *
 OPS_ParseUniaxialMaterialCommand(const char *matType) {
-  UniaxialMaterial  *theMaterial = 0;
 
+  if (initDone == false) {
+    OPS_SetUpUniaxialMaterials();
+    initDone = true;
+  }
+
+  UniaxialMaterial  *theMaterial = 0;
+  OPS_ParsingFunctionMap::const_iterator iter = uniaxialMaterialsMap.find(matType);
+  if (iter == uniaxialMaterialsMap.end()) {
+    return 0;
+  }
+
+  void *theMat = (*iter->second)();
+  if (theMat != 0) 
+    theMaterial = (UniaxialMaterial *)theMat;
+  else 
+    return 0;
+
+  return theMaterial;
+
+  /* OLD CODE .. IF ELSE
   if (strcmp(matType,"Elastic") == 0) {
     
     void *theMat = OPS_ElasticMaterial();
@@ -33,6 +73,6 @@ OPS_ParseUniaxialMaterialCommand(const char *matType) {
     else 
       return 0;
   }
-
   return theMaterial;
+  */
 }
