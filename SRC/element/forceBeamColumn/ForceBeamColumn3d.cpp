fc7c179e1c387a2e05c672abf9b69186e71a07e8
mhscott <Michael.Scott@oregonstate.edu>
2015-09-14 23:01:16 +0000

    OPS_NewForceBeamColumn2d and OPS_NewForceBeamColumn3d

--- SRC/element/forceBeamColumn/ForceBeamColumn3d.cpp
+++ SRC/element/forceBeamColumn/ForceBeamColumn3d.cpp
@@ -72,7 +72,8 @@ Journal of Structural Engineering, Approved for publication, February 2007.
 #include <FEM_ObjectBroker.h>
 #include <Renderer.h>
 #include <math.h>
-
+#include <elementAPI.h>
+#include <string>
 #include <fstream>
 using std::ifstream;
 #include <fstream>
@@ -96,6 +97,78 @@ double ForceBeamColumn3d::workArea[200];
 Vector *ForceBeamColumn3d::vsSubdivide = 0;
 Matrix *ForceBeamColumn3d::fsSubdivide = 0;
 Vector *ForceBeamColumn3d::SsrSubdivide = 0;
+
+void* OPS_NewForceBeamColumn3d()
+{
+    if(OPS_GetNumRemainingInputArgs() < 5) {
+	opserr<<"insufficient arguments:eleTag,iNode,jNode,transfTag,integrationTag\n";
+	return 0;
+    }
+
+    int ndm = OPS_GetNDM();
+    int ndf = OPS_GetNDF();
+    if(ndm != 3 || ndf != 6) {
+	opserr<<"ndm must be 3 and ndf must be 6\n";
+	return 0;
+    }
+
+    // inputs: 
+    int iData[5];
+    int numData = 5;
+    if(OPS_GetIntInput(&numData,&iData[0]) < 0) return 0;
+
+    // options
+    double mass = 0.0, tol=1e-12;
+    int maxIter = 10;
+    numData = 1;
+    while(OPS_GetNumRemainingInputArgs() > 0) {
+	std::string type = OPS_GetString();
+	if(type == "-iter") {
+	    if(OPS_GetNumRemainingInputArgs() > 1) {
+		if(OPS_GetIntInput(&numData,&maxIter) < 0) return 0;
+		if(OPS_GetDoubleInput(&numData,&tol) < 0) return 0;
+	    }
+	} else if(type == "-mass") {
+	    if(OPS_GetNumRemainingInputArgs() > 0) {
+		if(OPS_GetDoubleInput(&numData,&mass) < 0) return 0;
+	    }
+	}
+    }
+
+    // check transf
+    CrdTransf* theTransf = OPS_GetCrdTransf(iData[3]);
+    if(theTransf == 0) {
+	opserr<<"coord transfomration not found\n";
+	return 0;
+    }
+
+    // check beam integrataion
+    BeamIntegrationRule* theRule = OPS_getBeamIntegrationRule(iData[4]);
+    if(theRule == 0) {
+	opserr<<"beam integration not found\n";
+	return 0;
+    }
+    BeamIntegration* bi = theRule->getBeamIntegration();
+    if(bi == 0) {
+	opserr<<"beam integration is null\n";
+	return 0;
+    }
+
+    // check sections
+    const ID& secTags = theRule->getSectionTags();
+    SectionForceDeformation* sections[secTags.Size()];
+    for(int i=0; i<secTags.Size(); i++) {
+	sections[i] = OPS_getSectionForceDeformation(secTags(i));
+	if(sections[i] == 0) {
+	    opserr<<"section "<<secTags(i)<<"not found\n";
+	    return 0;
+	}
+    }
+
+    return new ForceBeamColumn3d(iData[0],iData[1],iData[2],secTags.Size(),sections,
+				 *bi,*theTransf,mass,maxIter,tol);
+}
+
 
 // constructor:
 // invoked by a FEM_ObjectBroker, recvSelf() needs to be invoked on this object.
