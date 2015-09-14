5bfddb74df46b315e85884cec4a507a8df395a90
mhscott <Michael.Scott@oregonstate.edu>
2015-09-14 22:16:18 +0000

    OPS_NewLinearCrdTransf2d and OPS_NewLinearCrdTransf3d

--- SRC/coordTransformation/LinearCrdTransf3d.cpp
+++ SRC/coordTransformation/LinearCrdTransf3d.cpp
@@ -39,12 +39,45 @@
 #include <Matrix.h>
 #include <Node.h>
 #include <Channel.h>
-
+#include <elementAPI.h>
+#include <string>
 #include <LinearCrdTransf3d.h>
 
 // initialize static variables
 Matrix LinearCrdTransf3d::Tlg(12,12);
 Matrix LinearCrdTransf3d::kg(12,12);
+
+void* OPS_NewLinearCrdTransf3d()
+{
+    if(OPS_GetNumRemainingInputArgs() < 4) {
+	opserr<<"insufficient arguments for LinearCrdTransf3d\n";
+	return 0;
+    }
+
+    // get tag
+    int tag;
+    int numData = 1;
+    if(OPS_GetIntInput(&numData,&tag) < 0) return 0;
+
+    // get vector
+    Vector vec(3);
+    double* vptr = &vec(0);
+    numData = 3;
+    if(OPS_GetDoubleInput(&numData,vptr) < 0) return 0;
+
+    // get option
+    Vector jntOffsetI(3), jntOffsetJ(3);
+    double *iptr=&jntOffsetI(0), *jptr=&jntOffsetJ(0);
+    while(OPS_GetNumRemainingInputArgs() > 6) {
+	std::string type = OPS_GetString();
+	if(type == "-jntOffset") {
+	    if(OPS_GetDoubleInput(&numData,iptr) < 0) return 0;
+	    if(OPS_GetDoubleInput(&numData,jptr) < 0) return 0;
+	}
+    }
+
+    return new LinearCrdTransf3d(tag,vec,jntOffsetI,jntOffsetJ);
+}
 
 
 // constructor:
