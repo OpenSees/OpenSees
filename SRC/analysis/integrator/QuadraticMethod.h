903d43d6201b36186c845f1da33e89d1db8bdc1e
M. Salehi <msa_imeg@yahoo.com>
2019-09-08 09:36:11 +0430

    Update QuadraticMethod.h

--- SRC/analysis/integrator/QuadraticMethod.h
+++ SRC/analysis/integrator/QuadraticMethod.h
@@ -19,25 +19,20 @@
 ** ****************************************************************** */
                                                                         
 // Written: M. Salehi opensees.net@gmail.com
-// website : http://opensees.net
 // Created: 02/19
 // Revision: A
 
 //refs
 //Structural Engineeringand Mechanics   Volume 48, Number 6, December25 2013, pages 849 - 878
 //DOI: https://doi.org/10.12989/sem.2013.48.6.849	
-//
 //Comprehensive evaluation of structural geometrical nonlinear solution techniques Part I : Formulation and characteristics of the methods
 //M.Rezaiee - Pajand, M.Ghalishooyan and M.Salehi - Ahmadabad
-//FULLTEXT : https://www.researchgate.net/publication/264146397_Comprehensive_evaluation_of_structural_geometrical_nonlinear_solution_techniques_Part_I_Formulation_and_characteristics_of_the_methods
 
 
 //Structural Engineeringand Mechanics   Volume 48, Number 6, December25 2013, pages 879 - 914
 //DOI: https://doi.org/10.12989/sem.2013.48.6.879	
-//
 //Comprehensive evaluation of structural geometrical nonlinear solution techniques Part II : Comparing efficiencies of the methods
 //M.Rezaiee - Pajand, M.Ghalishooyan and M.Salehi - Ahmadabad
-//FULLTEXT : https://www.researchgate.net/publication/263361974_Comprehensive_evaluation_of_structural_geometrical_nonlinear_solution_techniques_Part_II_Comparing_efficiencies_of_the_methods
 
 
 #ifndef QuadraticMethod_h
