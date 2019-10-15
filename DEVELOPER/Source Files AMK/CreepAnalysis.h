6cbfb427023a13fc453d92251fbfc9d6a8409b56
ntosic87 <54485694+ntosic87@users.noreply.github.com>
2019-10-15 06:27:44 +0200

    Add files via upload

---
+++ DEVELOPER/Source Files AMK/CreepAnalysis.h
@@ -0,0 +1,116 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 1999, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+                                                                        
+// $Revision: 1.8 $
+// $Date: 2009-05-11 21:32:27 $
+// $Source: /usr/local/cvs/OpenSees/SRC/analysis/analysis/CreepAnalysis.h,v $
+                                                                        
+                                                                        
+#ifndef CreepAnalysis_h
+#define CreepAnalysis_h
+
+// Written: fmk 
+// Revision: A
+//
+// Description: This file contains the interface for the CreepAnalysis
+// class. CreepAnalysis is a subclass of Analysis, it is used to perform 
+// a Creep analysis on the FE\_Model.
+//
+// What: "@(#) CreepAnalysis.h, revA"
+
+#include <Analysis.h>
+
+
+// AddingSensitivity:BEGIN //////////////////////////////////
+#ifdef _RELIABILITY
+#include <SensitivityAlgorithm.h>
+#endif
+// AddingSensitivity:END ////////////////////////////////////
+
+
+class ConstraintHandler;
+class DOF_Numberer;
+class AnalysisModel;
+class StaticIntegrator;
+class LinearSOE;
+class EquiSolnAlgo;
+class ConvergenceTest;
+class EigenSOE;
+
+class CreepAnalysis: public Analysis
+{
+  public:
+    CreepAnalysis(Domain &theDomain,
+		   ConstraintHandler &theHandler,
+		   DOF_Numberer &theNumberer,
+		   AnalysisModel &theModel,
+		   EquiSolnAlgo &theSolnAlgo,		   
+		   LinearSOE &theSOE,
+		   StaticIntegrator &theIntegrator,
+		   ConvergenceTest *theTest = 0);
+
+    ~CreepAnalysis();
+
+    void clearAll(void);	    
+    
+    int analyze(int numSteps);
+    int eigen(int numMode, bool generalized, bool findSmallest);
+    int initialize(void);
+    int domainChanged(void);
+
+    int setNumberer(DOF_Numberer &theNumberer);
+    int setAlgorithm(EquiSolnAlgo &theAlgorithm);
+    int setIntegrator(StaticIntegrator &theIntegrator);
+    int setLinearSOE(LinearSOE &theSOE);
+    int setConvergenceTest(ConvergenceTest &theTest);
+    int setEigenSOE(EigenSOE &theSOE);
+
+    EquiSolnAlgo     *getAlgorithm(void);
+    StaticIntegrator *getIntegrator(void);
+    ConvergenceTest  *getConvergenceTest(void);
+
+    // AddingSensitivity:BEGIN ///////////////////////////////
+#ifdef _RELIABILITY
+    int setSensitivityAlgorithm(SensitivityAlgorithm *theSensitivityAlgorithm);
+#endif
+    // AddingSensitivity:END /////////////////////////////////
+    
+  protected: 
+    
+  private:
+    ConstraintHandler 	*theConstraintHandler;    
+    DOF_Numberer 	*theDOF_Numberer;
+    AnalysisModel 	*theAnalysisModel;
+    EquiSolnAlgo 	*theAlgorithm;
+    LinearSOE 		*theSOE;
+    EigenSOE 		*theEigenSOE;
+    StaticIntegrator    *theIntegrator;
+    ConvergenceTest     *theTest;
+
+    int domainStamp;
+
+    // AddingSensitivity:BEGIN ///////////////////////////////
+#ifdef _RELIABILITY
+    SensitivityAlgorithm *theSensitivityAlgorithm;
+#endif
+    // AddingSensitivity:END ///////////////////////////////
+};
+
+#endif
---
+++ DEVELOPER/Source Files AMK/TDConcreteEXP.h
@@ -0,0 +1,165 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 1999, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+   
+ // Written: AMK
+ // Created: 2012
+ //
+ // Description: This file contains the class definition for TDConcreteEXP. 
+ // TDConcreteEXP is a time-dependent concrete material model that calculates
+ // creep and shrinkage strains.
+ /*-------------------------------
+ ! Concrete Compression - Linear
+ ! Concrete Tension - Tamai et al. (1988) "Average Stress-Strain Relationship in Post Yield Range of Steel Bar in Concrete"
+ ! Concrete Creep - Experimental Linear superposition of creep coefficient, ACI 209 time function
+ ! Concrete Shrinkage - ACI 209 time function
+ -------------------------------*/
+ //
+ // The framework for this code was originally modified from Concrete02.
+
+#ifndef TDConcreteEXP_h
+#define TDConcreteEXP_h
+
+#include <UniaxialMaterial.h>
+#include <Domain.h> //Added by AMK
+
+class TDConcreteEXP : public UniaxialMaterial
+{
+  public:
+    TDConcreteEXP(int tag, double _fc, double _ft, double _Ec, double _beta, double _age, double _epsshu, double _epssha, double _tcr, double _epscru, double _sigCr, double _epscra, double _epscrd, double _tcast);
+
+    TDConcreteEXP(void);
+
+    virtual ~TDConcreteEXP();
+
+    const char *getClassType(void) const {return "TDConcreteEXP";};    
+    double getInitialTangent(void);
+    UniaxialMaterial *getCopy(void);
+
+    int setTrialStrain(double strain, double strainRate = 0.0); 
+	double setCreepStrain(double time, double stress); //Added by AMK
+    double setStress(double strain, double &stiff); //Added by AMK
+	double getCurrentTime(void); //Added by AMK
+    double getStrain(void);
+    double getPHI_i(void); //Added by AMK      
+    double getStress(void);
+    double getTangent(void);
+	double getCreep(void); //Added by AMK
+	double getMech(void); //Added by AMK
+	double setPhi(double time, double tp); //Added by AMK
+	double setShrink(double time); //Added by AMK
+	double getShrink(void); //Added by AMK
+    double getKappa(void);
+    
+    int commitState(void);
+    int revertToLastCommit(void);    
+    int revertToStart(void);        
+    
+    int sendSelf(int commitTag, Channel &theChannel);  
+    int recvSelf(int commitTag, Channel &theChannel, 
+		 FEM_ObjectBroker &theBroker);    
+    
+    void Print(OPS_Stream &s, int flag =0);
+
+    int getVariable(const char *variable, Information &);
+	
+	
+	//Added by AMK for recording Creep and Mechanical Strain:
+	Response *setResponse(const char **argv, int argc,OPS_Stream &theOutput);
+	int getResponse(int responseID, Information &matInfo);
+    
+ protected:
+    
+ private:
+    void Tens_Envlp (double epsc, double &sigc, double &Ect);
+    void Compr_Envlp (double epsc, double &sigc, double &Ect);
+
+    // matpar : Concrete FIXED PROPERTIES
+    double fc;    // concrete compression strength           : mp(1)
+    //	double fcT;  //Time Dependent Strength
+    double epsc0; // strain at compression strength          : mp(2)
+    double fcu;   // stress at ultimate (crushing) strain    : mp(3)
+    double epscu; // ultimate (crushing) strain              : mp(4)       
+    double tcr;   // creep relationship age
+    double ft;    // concrete tensile strength               : mp(6)
+    //	double ftT;	//Time dependent strength (tension)
+    double Ets;   // tension stiffening slope                : mp(7)
+	double Ec;  //Concrete stiffness, Added by AMK
+	//	double EcT; //Time dependent stiffness
+	double age;   // concrete age at first loading, Added by AMK
+	double epsshu; // ultimate shrinkage
+    double epssha; // shrinkage parameter
+    double epscra; // concrete creep exponent parameter
+    double epscru; // ultimate concrete creep
+    double sigCr; // stress that creep curve is based on
+    double beta;
+    double epscrd;
+    double tcast;
+
+    // hstvP : Concerete HISTORY VARIABLES last committed step
+    double ecminP;  //  hstP(1)
+    double ecmaxP;  // added by AMK
+    double deptP;   //  hstP(2)
+    double epsP;  //  = strain at previous converged step
+    double sigP;  //  = stress at previous converged step
+    double eP;    //   stiffness modulus at last converged step;
+
+    // hstv : Concerete HISTORY VARIABLES  current step
+    double ecmin;
+    double ecmax; // added by AMK  
+    double dept;   
+    double sig;   
+    double e;     
+    double eps;
+	
+	//Added by AMK:
+	int count;
+	double epsInit;
+	double sigInit;
+	double eps_cr;
+	double eps_sh;
+	double eps_T;
+	double eps_m;
+	double epsP_m;
+	double epsP_cr;
+    double epsP_sh;
+	double eps_total;
+	double epsP_total;
+	double e_total;
+	double eP_total;
+	double t; //Time
+	double t_load; //loaded time
+	double phi_i;
+	double Et;
+	int crack_flag;
+	int crackP_flag;
+    int iter;
+    
+    float PHI_i[5000];
+    //float E_i[5000];
+    float DSIG_i[5000];
+    float dsig_i[5000];
+    float TIME_i[5000]; //Time from the previous time step
+    float DTIME_i[5000];
+    float kappa[5000];
+};
+
+
+#endif
+
