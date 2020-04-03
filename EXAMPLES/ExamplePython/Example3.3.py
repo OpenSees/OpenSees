11391eea1a29ee1674dc8dbf006d891a70288e05
Andreas Schellenberg <andreas.schellenberg@gmail.com>
2020-04-02 18:04:24 -0700

    updating py examples

--- EXAMPLES/ExamplePython/Example3.3.py
+++ EXAMPLES/ExamplePython/Example3.3.py
@@ -21,7 +21,7 @@
 # Date: June 2017
 
 # import the OpenSees Python module
-import opensees as ops
+from opensees import *
 import math
 
 # ----------------------------------------------------
@@ -29,13 +29,13 @@ import math
 # ----------------------------------------------------
 
 # remove existing model
-ops.wipe()
+wipe()
 
 # create ModelBuilder (with two-dimensions and 3 DOF/node)
-ops.model("BasicBuilder", "-ndm",2, "-ndf",3)
+model("BasicBuilder", "-ndm",2, "-ndf",3)
 
 # set default units
-ops.defaultUnits("-force", "kip", "-length", "in", "-time", "sec", "-temp", "F")
+defaultUnits("-force", "kip", "-length", "in", "-time", "sec", "-temp", "F")
 
 # Create nodes
 # ------------
@@ -44,29 +44,29 @@ width = 360.0
 height = 144.0
 
 # create nodes & add to Domain - command: node nodeId xCrd yCrd
-ops.node(1, 0.0,   0.0)
-ops.node(2, width, 0.0)
-ops.node(3, 0.0,   height)
-ops.node(4, width, height)
+node(1, 0.0,   0.0)
+node(2, width, 0.0)
+node(3, 0.0,   height)
+node(4, width, height)
 
 # set the boundary conditions - command: fix nodeID uxRestrnt? uyRestrnt? rzRestrnt?
-ops.fix(1, 1, 1, 1)
-ops.fix(2, 1, 1, 1)
+fix(1, 1, 1, 1)
+fix(2, 1, 1, 1)
 
 # Define materials for nonlinear columns
 # ------------------------------------------
 # CONCRETE                        tag  f'c    ec0    f'cu   ecu
 # Core concrete (confined)
-ops.uniaxialMaterial("Concrete01", 1, -6.0, -0.004, -5.0, -0.014)
+uniaxialMaterial("Concrete01", 1, -6.0, -0.004, -5.0, -0.014)
 # Cover concrete (unconfined)
-ops.uniaxialMaterial("Concrete01", 2, -5.0, -0.002, -0.0, -0.006)
+uniaxialMaterial("Concrete01", 2, -5.0, -0.002, -0.0, -0.006)
 
 # STEEL
 # Reinforcing steel 
 fy = 60.0;      # Yield stress
 E = 30000.0;    # Young's modulus
 #                              tag fy  E0  b
-ops.uniaxialMaterial("Steel01", 3, fy, E, 0.01)
+uniaxialMaterial("Steel01", 3, fy, E, 0.01)
 
 # Define cross-section for nonlinear columns
 # ------------------------------------------
@@ -80,43 +80,43 @@ As = 0.60;     # area of no. 7 bars
 y1 = colDepth/2.0
 z1 = colWidth/2.0
 
-ops.section("Fiber", 1)
+section("Fiber", 1)
 # Create the concrete core fibers
-ops.patch("rect", 1, 10, 1, cover-y1, cover-z1, y1-cover, z1-cover)
+patch("rect", 1, 10, 1, cover-y1, cover-z1, y1-cover, z1-cover)
 # Create the concrete cover fibers (top, bottom, left, right)
-ops.patch("rect", 2, 10, 1, -y1, z1-cover, y1, z1)
-ops.patch("rect", 2, 10, 1, -y1, -z1, y1, cover-z1)
-ops.patch("rect", 2,  2, 1, -y1, cover-z1, cover-y1, z1-cover)
-ops.patch("rect", 2,  2, 1,  y1-cover, cover-z1, y1, z1-cover)
+patch("rect", 2, 10, 1, -y1, z1-cover, y1, z1)
+patch("rect", 2, 10, 1, -y1, -z1, y1, cover-z1)
+patch("rect", 2,  2, 1, -y1, cover-z1, cover-y1, z1-cover)
+patch("rect", 2,  2, 1,  y1-cover, cover-z1, y1, z1-cover)
 # Create the reinforcing fibers (left, middle, right)
-ops.layer("straight", 3, 3, As, y1-cover, z1-cover, y1-cover, cover-z1)
-ops.layer("straight", 3, 2, As, 0.0, z1-cover, 0.0, cover-z1)
-ops.layer("straight", 3, 3, As, cover-y1, z1-cover, cover-y1, cover-z1)
+layer("straight", 3, 3, As, y1-cover, z1-cover, y1-cover, cover-z1)
+layer("straight", 3, 2, As, 0.0, z1-cover, 0.0, cover-z1)
+layer("straight", 3, 3, As, cover-y1, z1-cover, cover-y1, cover-z1)
 # define beam integration
 np = 5;  # number of integration points along length of element
-ops.beamIntegration("Lobatto", 1, 1, np)
+beamIntegration("Lobatto", 1, 1, np)
 
 # Define column elements
 # ----------------------
 # Geometric transformation for columns
 #                       tag 
-ops.geomTransf("PDelta", 1)
+geomTransf("PDelta", 1)
 
 # Create the column elements
 eleType = "forceBeamColumn"
 #                   tag ndI ndJ transfTag integrationTag
-ops.element(eleType, 1, 1, 3, 1, 1)
-ops.element(eleType, 2, 2, 4, 1, 1)
+element(eleType, 1, 1, 3, 1, 1)
+element(eleType, 2, 2, 4, 1, 1)
 
 # Define beam element
 # ------------------
 # Geometric transformation for beams
 #                tag 
-ops.geomTransf("Linear", 2)
+geomTransf("Linear", 2)
 
 # Create the beam element
 #                               tag ndI ndJ  A     E       Iz   transfTag
-ops.element("elasticBeamColumn", 3, 3, 4, 360.0, 4030.0, 8640.0, 2)
+element("elasticBeamColumn", 3, 3, 4, 360.0, 4030.0, 8640.0, 2)
 
 # Define gravity loads
 # --------------------
@@ -124,14 +124,14 @@ ops.element("elasticBeamColumn", 3, 3, 4, 360.0, 4030.0, 8640.0, 2)
 P = 180.0;                # 10% of axial capacity of columns
 
 # create a Linear TimeSeries (load factor varies linearly with time) - command: timeSeries Linear $tag
-ops.timeSeries("Linear", 1)
+timeSeries("Linear", 1)
 
 # create a Plain load pattern - command: pattern Plain $tag $timeSeriesTag { $loads }
-ops.pattern("Plain", 1, 1, "-fact", 1.0)
+pattern("Plain", 1, 1, "-fact", 1.0)
 
 # create the nodal load - command: load nodeID xForce yForce zMoment
-ops.load(3, 0.0, -P, 0.0)
-ops.load(4, 0.0, -P, 0.0)
+load(3, 0.0, -P, 0.0)
+load(4, 0.0, -P, 0.0)
 
 # ------------------------------
 # End of model generation
@@ -143,26 +143,26 @@ ops.load(4, 0.0, -P, 0.0)
 # ------------------------------
 
 # create the system of equation
-ops.system("BandGeneral")
+system("BandGeneral")
 
 # create the DOF numberer, the reverse Cuthill-McKee algorithm
-ops.numberer("RCM")
+numberer("RCM")
 
 # create the constraint handler, a Plain handler is used as homo constraints
-ops.constraints("Transformation")
+constraints("Transformation")
 
 # Create the convergence test, the norm of the residual with a tolerance of 
 # 1e-12 and a max number of iterations of 10
-ops.test("NormDispIncr", 1.0E-12, 10, 3)
+test("NormDispIncr", 1.0E-12, 10, 3)
 
 # create the solution algorithm, a Newton-Raphson algorithm
-ops.algorithm("Newton")
+algorithm("Newton")
 
 # create the integration scheme, the LoadControl scheme using steps of 0.1
-ops.integrator("LoadControl", 0.1)
+integrator("LoadControl", 0.1)
 
 # create the analysis object 
-ops.analysis("Static")
+analysis("Static")
 
 # ------------------------------
 # End of analysis generation
@@ -174,12 +174,12 @@ ops.analysis("Static")
 # ------------------------------
 
 # perform the gravity load analysis, requires 10 steps to reach the load level
-ops.analyze(10)
+analyze(10)
 
 print("Gravity load analysis completed\n")
 
 # Set the gravity loads to be constant & reset the time in the domain
-ops.loadConst("-time", 0.0)
+loadConst("-time", 0.0)
 
 # ----------------------------------------------------
 # End of Model Generation & Initial Gravity Analysis
@@ -194,8 +194,8 @@ ops.loadConst("-time", 0.0)
 m = P/g
 
 #       tag MX MY RZ
-ops.mass(3, m, m, 0.0)
-ops.mass(4, m, m, 0.0)
+mass(3, m, m, 0.0)
+mass(4, m, m, 0.0)
 
 # Define dynamic loads
 # --------------------
@@ -204,14 +204,14 @@ gmFile = "ARL360.g3"
 dt = 0.02
 
 # Set time series to be passed to uniform excitation
-ops.timeSeries("Path", 2, "-filePath", gmFile, "-dt", dt, "-factor", g)
+timeSeries("Path", 2, "-filePath", gmFile, "-dt", dt, "-factor", g)
 
 # Create UniformExcitation load pattern
 #                               tag dir        tsTag
-ops.pattern("UniformExcitation", 2, 1, "-accel", 2)
+pattern("UniformExcitation", 2, 1, "-accel", 2)
 
 # Set the rayleigh damping factors for nodes & elements
-ops.rayleigh(0.0, 0.0, 0.0, 0.000625)
+rayleigh(0.0, 0.0, 0.0, 0.000625)
 
 # ----------------------------------------------------
 # End of additional modelling for dynamic loads
@@ -223,29 +223,29 @@ ops.rayleigh(0.0, 0.0, 0.0, 0.000625)
 # ---------------------------------------------------------
 
 # delete the old analysis and all its component objects
-ops.wipeAnalysis()
+wipeAnalysis()
 
 # create the system of equation, a banded general storage scheme
-ops.system("BandGeneral")
+system("BandGeneral")
 
 # create the DOF numberer, the reverse Cuthill-McKee algorithm
-ops.numberer("RCM")
+numberer("RCM")
 
 # create the constraint handler, a plain handler as homogeneous boundary
-ops.constraints("Plain")
+constraints("Plain")
 
 # create the convergence test, the norm of the residual with a tolerance of 
 # 1e-12 and a max number of iterations of 10
-ops.test("NormDispIncr", 1.0E-12, 10)
+test("NormDispIncr", 1.0E-12, 10)
 
 # create the solution algorithm, a Newton-Raphson algorithm
-ops.algorithm("Newton")
+algorithm("Newton")
 
 # create the integration scheme, the Newmark with gamma=0.5 and beta=0.25
-ops.integrator("Newmark", 0.5, 0.25) 
+integrator("Newmark", 0.5, 0.25) 
 
 # create the analysis object 
-ops.analysis("Transient")
+analysis("Transient")
 
 # ---------------------------------------------------------
 # End of modifications to analysis for transient analysis
@@ -257,14 +257,14 @@ ops.analysis("Transient")
 # ------------------------------
 
 # Create a recorder to monitor nodal displacements
-ops.recorder("Node", "-time", "-file", "disp.out", "-node", 3, 4, "-dof", 1, 2, 3, "disp")
-ops.recorder("Node", "-time", "-file", "accel.out", "-node", 3, 4, "-dof", 1, 2, 3, "accel")
-ops.recorder("Node", "-time", "-file", "totAccel.out", "-timeSeries", 2, 0, 0, "-node", 3, 4, "-dof", 1, 2, 3, "accel")
+recorder("Node", "-time", "-file", "disp.out", "-node", 3, 4, "-dof", 1, 2, 3, "disp")
+recorder("Node", "-time", "-file", "accel.out", "-node", 3, 4, "-dof", 1, 2, 3, "accel")
+recorder("Node", "-time", "-file", "totAccel.out", "-timeSeries", 2, 0, 0, "-node", 3, 4, "-dof", 1, 2, 3, "accel")
 
 # Create recorders to monitor section forces and deformations
 # at the base of the left column
-ops.recorder("Element", "-time", "-file", "ele1secForce.out", "-ele", 1, "section", 1, "force")
-ops.recorder("Element", "-time", "-file", "ele1secDef.out", "-ele", 1, "section", 1, "deformation")
+recorder("Element", "-time", "-file", "ele1secForce.out", "-ele", 1, "section", 1, "force")
+recorder("Element", "-time", "-file", "ele1secDef.out", "-ele", 1, "section", 1, "deformation")
 
 # --------------------------------
 # End of recorder generation
@@ -276,35 +276,35 @@ ops.recorder("Element", "-time", "-file", "ele1secDef.out", "-ele", 1, "section"
 # ------------------------------
 
 # record once at time 0
-ops.record()
+record()
 
 # Perform an eigenvalue analysis
-lam = ops.eigen(2)
+lam = eigen(2)
 Tstart = 2.0*math.pi/math.sqrt(lam[0])
 print("Fundamental period at start of transient analysis: ", Tstart, "sec\n")
 
 # set some variables
 tFinal = 2000 * 0.01
-tCurrent = ops.getTime()
+tCurrent = getTime()
 ok = 0
 
 # Perform the transient analysis
 while ((ok == 0) and (tCurrent < tFinal)):
     
-    ok = ops.analyze(1, 0.01)
+    ok = analyze(1, 0.01)
     
     # if the analysis fails try initial tangent iteration
     if (ok != 0):
         print("regular newton failed .. lets try an initial stiffness for this step")
-        ops.test("NormDispIncr", 1.0E-12, 100, 0)
-        ops.algorithm("ModifiedNewton", "-initial")
-        ok = ops.analyze(1, 0.01)
+        test("NormDispIncr", 1.0E-12, 100, 0)
+        algorithm("ModifiedNewton", "-initial")
+        ok = analyze(1, 0.01)
         if (ok == 0):
             print("that worked .. back to regular newton")
-        ops.test("NormDispIncr", 1.0E-12, 10) 
-        ops.algorithm("Newton")
+        test("NormDispIncr", 1.0E-12, 10) 
+        algorithm("Newton")
     
-    tCurrent = ops.getTime()
+    tCurrent = getTime()
 
 # Print a message to indicate if analysis successful or not
 if (ok == 0):
@@ -313,10 +313,10 @@ else:
     print("\nTransient analysis FAILED\n")
 
 # Perform an eigenvalue analysis
-lam = ops.eigen(2)
+lam = eigen(2)
 Tend = 2.0*math.pi/math.sqrt(lam[0])
 print("Fundamental period at end of transient analysis: ", Tend, "sec")
 
 # Print state of node 3
-ops.printModel("node", 3)
-ops.wipe()
+printModel("node", 3)
+wipe()
--- EXAMPLES/ExamplePython/Example4.1.py
+++ EXAMPLES/ExamplePython/Example4.1.py
@@ -19,7 +19,7 @@
 # Date: August 2017
 
 # import the OpenSees Python module
-import opensees as ops
+from opensees import *
 import math
 
 # ------------------------------
@@ -30,13 +30,13 @@ import math
 numBay = 3
 
 # remove existing model
-ops.wipe()
+wipe()
 
 # create ModelBuilder (with two-dimensions and 3 DOF/node)
-ops.model("BasicBuilder", "-ndm",2, "-ndf",3)
+model("BasicBuilder", "-ndm",2, "-ndf",3)
 
 # set default units
-ops.defaultUnits("-force", "kip", "-length", "in", "-time", "sec", "-temp", "F")
+defaultUnits("-force", "kip", "-length", "in", "-time", "sec", "-temp", "F")
 
 # Create nodes
 # ------------
@@ -50,73 +50,73 @@ for i in range(numBay+1):
     xDim = i * bayWidth
     
     #         tag       X      Y
-    ops.node(nodeID,   xDim,   0.0)
-    ops.node(nodeID+1, xDim, 180.0, "-mass", m, m, 0.0)
-    ops.node(nodeID+2, xDim, 324.0, "-mass", m, m, 0.0)
+    node(nodeID,   xDim,   0.0)
+    node(nodeID+1, xDim, 180.0, "-mass", m, m, 0.0)
+    node(nodeID+2, xDim, 324.0, "-mass", m, m, 0.0)
 
     nodeID += 3
 
 # Fix supports at base of columns
 for i in range(numBay+1):
     #       node  DX DY RZ
-    ops.fix(i*3+1, 1, 1, 1)
+    fix(i*3+1, 1, 1, 1)
 
 # Define materials for nonlinear columns
 # ------------------------------------------
 # CONCRETE                        tag  f'c    ec0    f'cu   ecu
 # Core concrete (confined)
-ops.uniaxialMaterial("Concrete01", 1, -6.0, -0.004, -5.0, -0.014)
+uniaxialMaterial("Concrete01", 1, -6.0, -0.004, -5.0, -0.014)
 # Cover concrete (unconfined)
-ops.uniaxialMaterial("Concrete01", 2, -5.0, -0.002, -0.0, -0.006)
+uniaxialMaterial("Concrete01", 2, -5.0, -0.002, -0.0, -0.006)
 
 # STEEL
 # Reinforcing steel 
 fy = 60.0;      # Yield stress
 E = 30000.0;    # Young's modulus
 #                              tag fy  E0  b
-ops.uniaxialMaterial("Steel01", 3, fy, E, 0.015)
+uniaxialMaterial("Steel01", 3, fy, E, 0.015)
 
 # Define cross-section for nonlinear columns
 # ------------------------------------------
 # Interior column section
-ops.section("Fiber", 1)
+section("Fiber", 1)
 #                mat nfIJ nfJK   yI     zI     yJ     zJ     yK     zK     yL     zL
-ops.patch("quad", 2,   1,  12, -11.5,  10.0, -11.5, -10.0,  11.5, -10.0,  11.5,  10.0)
-ops.patch("quad", 1,   1,  14, -13.5, -10.0, -13.5, -12.0,  13.5, -12.0,  13.5, -10.0)
-ops.patch("quad", 1,   1,  14, -13.5,  12.0, -13.5,  10.0,  13.5,  10.0,  13.5,  12.0)
-ops.patch("quad", 1,   1,   2, -13.5,  10.0, -13.5, -10.0, -11.5, -10.0, -11.5,  10.0)
-ops.patch("quad", 1,   1,   2,  11.5,  10.0,  11.5, -10.0,  13.5, -10.0,  13.5,  10.0)
+patch("quad", 2,   1,  12, -11.5,  10.0, -11.5, -10.0,  11.5, -10.0,  11.5,  10.0)
+patch("quad", 1,   1,  14, -13.5, -10.0, -13.5, -12.0,  13.5, -12.0,  13.5, -10.0)
+patch("quad", 1,   1,  14, -13.5,  12.0, -13.5,  10.0,  13.5,  10.0,  13.5,  12.0)
+patch("quad", 1,   1,   2, -13.5,  10.0, -13.5, -10.0, -11.5, -10.0, -11.5,  10.0)
+patch("quad", 1,   1,   2,  11.5,  10.0,  11.5, -10.0,  13.5, -10.0,  13.5,  10.0)
 #                    mat nBars area    yI    zI     yF    zF
-ops.layer("straight", 3,   6,  1.56, -10.5,  9.0, -10.5, -9.0)
-ops.layer("straight", 3,   6,  1.56,  10.5,  9.0,  10.5, -9.0)
+layer("straight", 3,   6,  1.56, -10.5,  9.0, -10.5, -9.0)
+layer("straight", 3,   6,  1.56,  10.5,  9.0,  10.5, -9.0)
 # define beam integration
 np = 4;  # number of integration points along length of element
-ops.beamIntegration("Lobatto", 1, 1, np)
+beamIntegration("Lobatto", 1, 1, np)
 
 # Exterior column section
-ops.section("Fiber", 2)
-ops.patch("quad", 2, 1, 10, -10.0,  10.0, -10.0, -10.0,  10.0, -10.0,  10.0,  10.0)
-ops.patch("quad", 1, 1, 12, -12.0, -10.0, -12.0, -12.0,  12.0, -12.0,  12.0, -10.0)
-ops.patch("quad", 1, 1, 12, -12.0,  12.0, -12.0,  10.0,  12.0,  10.0,  12.0,  12.0)
-ops.patch("quad", 1, 1,  2, -12.0,  10.0, -12.0, -10.0, -10.0, -10.0, -10.0,  10.0)
-ops.patch("quad", 1, 1,  2,  10.0,  10.0,  10.0, -10.0,  12.0, -10.0,  12.0,  10.0)
-ops.layer("straight", 3, 6, 0.79, -9.0, 9.0, -9.0, -9.0)
-ops.layer("straight", 3, 6, 0.79,  9.0, 9.0,  9.0, -9.0)
+section("Fiber", 2)
+patch("quad", 2, 1, 10, -10.0,  10.0, -10.0, -10.0,  10.0, -10.0,  10.0,  10.0)
+patch("quad", 1, 1, 12, -12.0, -10.0, -12.0, -12.0,  12.0, -12.0,  12.0, -10.0)
+patch("quad", 1, 1, 12, -12.0,  12.0, -12.0,  10.0,  12.0,  10.0,  12.0,  12.0)
+patch("quad", 1, 1,  2, -12.0,  10.0, -12.0, -10.0, -10.0, -10.0, -10.0,  10.0)
+patch("quad", 1, 1,  2,  10.0,  10.0,  10.0, -10.0,  12.0, -10.0,  12.0,  10.0)
+layer("straight", 3, 6, 0.79, -9.0, 9.0, -9.0, -9.0)
+layer("straight", 3, 6, 0.79,  9.0, 9.0,  9.0, -9.0)
 # define beam integration
-ops.beamIntegration("Lobatto", 2, 2, np)
+beamIntegration("Lobatto", 2, 2, np)
 
 # Girder section
-ops.section("Fiber", 3)
-ops.patch("quad", 1, 1, 12, -12.0, 9.0, -12.0, -9.0, 12.0, -9.0, 12.0, 9.0)
-ops.layer("straight", 3, 4, 1.0, -9.0, 9.0, -9.0, -9.0)
-ops.layer("straight", 3, 4, 1.0,  9.0, 9.0,  9.0, -9.0)
+section("Fiber", 3)
+patch("quad", 1, 1, 12, -12.0, 9.0, -12.0, -9.0, 12.0, -9.0, 12.0, 9.0)
+layer("straight", 3, 4, 1.0, -9.0, 9.0, -9.0, -9.0)
+layer("straight", 3, 4, 1.0,  9.0, 9.0,  9.0, -9.0)
 # define beam integration
-ops.beamIntegration("Lobatto", 3, 3, np)
+beamIntegration("Lobatto", 3, 3, np)
 
 # Define column elements
 # ----------------------
 # Geometric transformation
-ops.geomTransf("Linear", 1)
+geomTransf("Linear", 1)
 
 beamID = 1
 eleType = "forceBeamColumn"
@@ -130,11 +130,11 @@ for i in range(numBay+1):
     for j in range(1, 3):
         # add the column element (secId == 2 if external, 1 if internal column)
         if (i == 0):
-            ops.element(eleType, beamID, iNode, jNode, 1, 2)
+            element(eleType, beamID, iNode, jNode, 1, 2)
         elif (i == numBay):
-            ops.element(eleType, beamID, iNode, jNode, 1, 2)
+            element(eleType, beamID, iNode, jNode, 1, 2)
         else:
-            ops.element(eleType, beamID, iNode, jNode, 1, 1)
+            element(eleType, beamID, iNode, jNode, 1, 1)
         
         # increment the parameters
         iNode += 1
@@ -144,7 +144,7 @@ for i in range(numBay+1):
 # Define beam elements
 # ----------------------
 # Geometric transformation
-ops.geomTransf("Linear", 2)
+geomTransf("Linear", 2)
 
 # Define elements
 for j in range(1, 3):
@@ -153,7 +153,7 @@ for j in range(1, 3):
     jNode = iNode + 3
 
     for i in range(1, numBay+1):
-        ops.element(eleType, beamID, iNode, jNode, 2, 3)
+        element(eleType, beamID, iNode, jNode, 2, 3)
         
         # increment the parameters
         iNode += 3
@@ -166,10 +166,10 @@ for j in range(1, 3):
 P = -192.0
 
 # create a Linear TimeSeries
-ops.timeSeries("Linear", 1)
+timeSeries("Linear", 1)
 
 # create a Plain load pattern
-ops.pattern("Plain", 1, 1, "-fact", 1.0)
+pattern("Plain", 1, 1, "-fact", 1.0)
 
 # Create nodal loads at nodes 
 for i in range(numBay+1):
@@ -178,18 +178,18 @@ for i in range(numBay+1):
     node2 = node1 + 1
     
     if (i == 0):
-        ops.load(node1, 0.0, P,     0.0) 
-        ops.load(node2, 0.0, P/2.0, 0.0)
+        load(node1, 0.0, P,     0.0) 
+        load(node2, 0.0, P/2.0, 0.0)
     elif (i == numBay):
-        ops.load(node1, 0.0, P,     0.0)
-        ops.load(node2, 0.0, P/2.0, 0.0)
+        load(node1, 0.0, P,     0.0)
+        load(node2, 0.0, P/2.0, 0.0)
     else:
-        ops.load(node1, 0.0, 2.0*P, 0.0)
-        ops.load(node2, 0.0, P,     0.0)
+        load(node1, 0.0, 2.0*P, 0.0)
+        load(node2, 0.0, P,     0.0)
 
 # print model
-#ops.printModel()
-ops.printModel("-JSON", "-file", "Example4.1.json")
+#printModel()
+printModel("-JSON", "-file", "Example4.1.json")
 
 # ------------------------------
 # End of model generation
@@ -201,26 +201,26 @@ ops.printModel("-JSON", "-file", "Example4.1.json")
 # --------------------------------------------------
 
 # create the system of equation
-ops.system("BandGeneral")
+system("BandGeneral")
 
 # create the DOF numberer, the reverse Cuthill-McKee algorithm
-ops.numberer("RCM")
+numberer("RCM")
 
 # create the constraint handler, a Plain handler is used as homo constraints
-ops.constraints("Plain")
+constraints("Plain")
 
 # Create the convergence test, the norm of the residual with a tolerance of 
 # 1e-12 and a max number of iterations of 10
-ops.test("NormDispIncr", 1.0E-8, 10, 0)
+test("NormDispIncr", 1.0E-8, 10, 0)
 
 # create the solution algorithm, a Newton-Raphson algorithm
-ops.algorithm("Newton")
+algorithm("Newton")
 
 # create the integration scheme, the LoadControl scheme using steps of 0.1
-ops.integrator("LoadControl", 0.1)
+integrator("LoadControl", 0.1)
 
 # create the analysis object 
-ops.analysis("Static")
+analysis("Static")
 
 # ------------------------------------------------
 # End of analysis generation for gravity analysis
@@ -232,16 +232,16 @@ ops.analysis("Static")
 # ------------------------------
 
 # initialize the model, done to set initial tangent
-ops.initialize()
+initialize()
 
 # perform the gravity load analysis, requires 10 steps to reach the load level
-ops.analyze(10)
+analyze(10)
 
 print("Gravity load analysis completed\n")
 
 # set gravity loads to be const and set pseudo time to be 0.0
 # for start of lateral load analysis
-ops.loadConst("-time", 0.0)
+loadConst("-time", 0.0)
 
 
 # ------------------------------
@@ -252,9 +252,9 @@ ops.loadConst("-time", 0.0)
 H = 10.0
 
 # Set lateral load pattern with a Linear TimeSeries
-ops.pattern("Plain", 2, 1, "-fact", 1.0)
-ops.load(2, H/2.0, 0.0, 0.0)
-ops.load(3, H,     0.0, 0.0)
+pattern("Plain", 2, 1, "-fact", 1.0)
+load(2, H/2.0, 0.0, 0.0)
+load(3, H,     0.0, 0.0)
 
 
 # ------------------------------
@@ -263,7 +263,7 @@ ops.load(3, H,     0.0, 0.0)
 
 # Create a recorder which writes to Node.out and prints
 # the current load factor (pseudo-time) and dof 1 displacements at node 2 & 3
-ops.recorder("Node", "-file", "Node41.out", "-time", "-node", 2, 3, "-dof", 1, "disp")
+recorder("Node", "-file", "Node41.out", "-time", "-node", 2, 3, "-dof", 1, "disp")
 
 # Source in some commands to display the model
 # comment out one of lines
@@ -272,7 +272,7 @@ displayMode = "displayOFF"
 
 if (displayMode == "displayON"):  
     # a window to plot the nodal displacements versus load for node 3  
-    ops.recorder("plot", "Node41.out", "Node_3_Xdisp", 10, 340, 300, 300, "-columns", 3, 1, "-dT", 0.1)
+    recorder("plot", "Node41.out", "Node_3_Xdisp", 10, 340, 300, 300, "-columns", 3, 1, "-dT", 0.1)
 
 # ------------------------------
 # End of recorder generation
@@ -284,31 +284,31 @@ if (displayMode == "displayON"):
 # ------------------------------
 
 # Perform an eigenvalue analysis
-lam = ops.eigen(2)
+lam = eigen(2)
 Tstart = 2.0*math.pi/math.sqrt(lam[0])
 print("Fundamental period at start of pushover analysis: ", Tstart, "sec\n")
 
 # Change the integrator to take a min and max load increment
-ops.integrator("LoadControl", 1.0, 4, 0.02, 2.0)
+integrator("LoadControl", 1.0, 4, 0.02, 2.0)
 
 # record once at time 0
-ops.record()
+record()
 
 # Perform the pushover analysis
 # Set some parameters
 maxU = 10.0;	        # Max displacement
 controlDisp = 0.0
-ok = ops.analyze(1)
+ok = analyze(1)
 while ((ok == 0) and (controlDisp < maxU)):
-    ok = ops.analyze(1)
-    controlDisp = ops.nodeDisp(3, 1)
+    ok = analyze(1)
+    controlDisp = nodeDisp(3, 1)
     if (ok != 0):
         print("... trying an initial tangent iteration")
-        ops.test("NormDispIncr", 1.0e-8, 4000, 0)
-        ops.algorithm("ModifiedNewton", "-initial")
-        ok = ops.analyze(1)
-        ops.test("NormDispIncr", 1.0e-8, 10, 0)
-        ops.algorithm("Newton")
+        test("NormDispIncr", 1.0e-8, 4000, 0)
+        algorithm("ModifiedNewton", "-initial")
+        ok = analyze(1)
+        test("NormDispIncr", 1.0e-8, 10, 0)
+        algorithm("Newton")
 
 # Print a message to indicate if analysis successful or not
 if (ok == 0):
@@ -317,5 +317,5 @@ else:
     print("\nPushover analysis FAILED\n")
 
 # Print the state at node 3
-ops.printModel("node", 3)
-ops.wipe()
+printModel("node", 3)
+wipe()
