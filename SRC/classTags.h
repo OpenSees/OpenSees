762d9fcdcb6808b4a1ba2d060a64d6da225dbde4
Jose Abell <ja.abell@gmail.com>
2020-07-17 14:21:08 -0400

    * Added IncrementalElasticIsotropicThreeDimensional.o to SRC/Makefile * Added missing classtag for the new beam column id element

--- SRC/classTags.h
+++ SRC/classTags.h
@@ -759,6 +759,7 @@
 #define ELE_TAG_PML2D                     202
 #define ELE_TAG_ASDShellQ4                203  // Massimo Petracca (ASDEA)
 #define ELE_TAG_ASDShellT3                204  // Massimo Petracca (ASDEA)
+#define ELE_TAG_DispBeamColumn3dID        205 // Jose Abell the Chileno added 
 #define ELE_TAG_ExternalElement           99990
 
 
