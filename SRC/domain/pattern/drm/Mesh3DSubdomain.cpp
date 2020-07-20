844b40f02aa8741c78a8c3151381241341ceca47
fmckenna <fmckenna@berkeley.edu>
2020-07-19 23:58:56 -0700

    fmk - cleaning up headers. moving stuff to .cpp files in drm

--- SRC/domain/pattern/drm/Mesh3DSubdomain.cpp
+++ SRC/domain/pattern/drm/Mesh3DSubdomain.cpp
@@ -1,4 +1,41 @@
 #include "Mesh3DSubdomain.h"
+#include <stdlib.h>
+#include <iostream>
+#include <OPS_Globals.h>
+#include <StandardStream.h>
+#include <Timer.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <NDMaterial.h>
+
+#include <ArrayOfTaggedObjects.h>
+
+// includes for the domain classes
+#include <Domain.h>
+#include <ShadowSubdomain.h>
+#include <ActorSubdomain.h>
+#include <Node.h>
+#include <Element.h>
+#include <ZeroLength.h>
+#include <ElasticMaterial.h>
+#include <ElasticIsotropicThreeDimensional.h>
+
+#include <SP_Constraint.h>
+
+#include <SingleDomAllSP_Iter.h>
+#include <NodeIter.h>
+#include <Vertex.h>
+#include <VertexIter.h>
+#include <Graph.h>
+#include <Vector.h>
+#include <Matrix.h>
+#include "GeometricBrickDecorator.h"
+#include <math.h>
+using namespace std;
+
+
 #include <Element.h>
 #include <Node.h>
 #include <DummyNode.h>
