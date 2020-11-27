41227ba844708a0dd91c4a08151fde471dd7b546
Jose Abell <ja.abell@gmail.com>
2020-11-27 16:22:40 -0300

    Updated PETSc solver for new API

--- SRC/actor/objectBroker/FEM_ObjectBrokerAllClasses.cpp
+++ SRC/actor/objectBroker/FEM_ObjectBrokerAllClasses.cpp
@@ -492,6 +492,7 @@
 
 #ifdef _PETSC
 #include "PetscSOE.h"
+#include "PetscSolver.h"
 #include "SparseGenColLinSOE.h"
 #endif
 
@@ -1591,6 +1592,9 @@ FEM_ObjectBrokerAllClasses::getNewLoadPattern(int classTag)
 	case PATTERN_TAG_DRMLoadPattern:
 	     return new DRMLoadPatternWrapper();
 
+        case PATTERN_TAG_H5DRM:
+             return new H5DRM();
+
 	default:
 	     opserr << "FEM_ObjectBrokerAllClasses::getPtrLoadPattern - ";
 	     opserr << " - no Load type exists for class tag ";
@@ -1639,6 +1643,9 @@ FEM_ObjectBrokerAllClasses::getNewTimeSeries(int classTag)
 
         case TSERIES_TAG_ConstantSeries:
 	  return new ConstantSeries;
+
+        case TSERIES_TAG_TriangleSeries:
+          return new TriangleSeries;
 
         case TSERIES_TAG_TrigSeries:
 	  return new TrigSeries;
@@ -1791,6 +1798,9 @@ FEM_ObjectBrokerAllClasses::getPtrNewRecorder(int classTag)
 
         case RECORDER_TAGS_TclFeViewer:  
 	  return 0;
+
+        case RECORDER_TAGS_GmshRecorder:
+           return new GmshRecorder();
 
         case RECORDER_TAGS_MPCORecorder:
           return new MPCORecorder();
@@ -1985,6 +1995,9 @@ FEM_ObjectBrokerAllClasses::getNewStaticIntegrator(int classTag)
 	case INTEGRATOR_TAGS_LoadControl:  
 	     return new LoadControl(1.0,1,1.0,.10); // must recvSelf
 
+    case INTEGRATOR_TAGS_StagedLoadControl:
+        return new StagedLoadControl(1.0, 1, 1.0, .10); // must recvSelf
+
 #ifdef _PARALLEL_PROCESSING
 	case INTEGRATOR_TAGS_DistributedDisplacementControl:  
 	     return new DistributedDisplacementControl(); // must recvSelf
@@ -2092,6 +2105,9 @@ FEM_ObjectBrokerAllClasses::getNewTransientIntegrator(int classTag)
     case INTEGRATOR_TAGS_Newmark:  
 	     return new Newmark();
 
+        case INTEGRATOR_TAGS_StagedNewmark:
+        return new StagedNewmark();
+
     case INTEGRATOR_TAGS_NewmarkExplicit:  
 	     return new NewmarkExplicit();
 
@@ -2183,17 +2199,19 @@ FEM_ObjectBrokerAllClasses::getNewLinearSOE(int classTagSOE)
 	  theSOE = new SparseGenColLinSOE();
 	  return theSOE;
 
-#ifdef _PETSC
-        case LinSOE_TAGS_PetscSOE:  
-	  theSOE = new PetscSOE();
-	  return theSOE;
-#endif
 
 #ifdef _PARALLEL_PROCESSING
 
 #ifdef _MUMPS
         case LinSOE_TAGS_MumpsParallelSOE:  
 	  theSOE = new MumpsParallelSOE();
+          return theSOE;
+#endif
+
+#ifdef _PETSC
+    case LinSOE_TAGS_PetscSOE:
+        thePetscSolver = new PetscSolver();
+        theSOE = new PetscSOE(*thePetscSolver);
 	  return theSOE;
 #endif
 
