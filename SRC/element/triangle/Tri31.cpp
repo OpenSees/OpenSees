9a84fddb8bca8897ca4e7d4728a1245ba3ca838a
ambaker1 <ambaker1@mtu.edu>
2021-02-09 23:46:19 -0500

    three-node triangle displays
    Updated or added three-node triangle displays, using getDisplayCrds

--- SRC/element/triangle/Tri31.cpp
+++ SRC/element/triangle/Tri31.cpp
@@ -1163,67 +1163,38 @@ Tri31::Print(OPS_Stream &s, int flag)
 int
 Tri31::displaySelf(Renderer &theViewer, int displayMode, float fact, const char **modes, int numMode)
 {
+	// get the end point display coords
+	static Vector v1(3);
+	static Vector v2(3);
+	static Vector v3(3);
+	theNodes[0]->getDisplayCrds(v1, fact, displayMode);
+	theNodes[1]->getDisplayCrds(v2, fact, displayMode);
+	theNodes[2]->getDisplayCrds(v3, fact, displayMode);
 
-    // first set the quantity to be displayed at the nodes;
-    // if displayMode is 1 through 3 we will plot material stresses otherwise 0.0
+	// place values in coords matrix
+	static Matrix coords(3, 3);
+	for (int i = 0; i < 3; i++) {
+		coords(0, i) = v1(i);
+		coords(1, i) = v2(i);
+		coords(2, i) = v3(i);
+	}
 
-    static Vector values(numgp);
+	// set the quantity to be displayed at the nodes;
+	// if displayMode is 1 through 3 we will plot material stresses otherwise 0.0
+	static Vector values(numgp);
+	if (displayMode < numgp && displayMode > 0) {
+		for (int i = 0; i < numgp; i++) {
+			const Vector& stress = theMaterial[i]->getStress();
+			values(i) = stress(displayMode - 1);
+		}
+	}
+	else {
+		for (int i = 0; i < numgp; i++)
+			values(i) = 0.0;
+	}
 
-    for (int j=0; j<numgp; j++) values(j) = 0.0;
-
-    if (displayMode < numgp && displayMode > 0) {
-		for (int i=0; i<numgp; i++) {
-			const Vector &stress = theMaterial[i]->getStress();
-	        values(i) = stress(displayMode-1);
-	    }
-    }
-
-    // now  determine the end points of the Tri31 based on
-    // the display factor (a measure of the distorted image)
-    // store this information in 3 3d vectors v1 through v3
-    const Vector &end1Crd = theNodes[0]->getCrds();
-    const Vector &end2Crd = theNodes[1]->getCrds();	
-    const Vector &end3Crd = theNodes[2]->getCrds();	
-
-    static Matrix coords(numnodes,3);
-
-    if (displayMode >= 0) {  
-
-		const Vector &end1Disp = theNodes[0]->getDisp();
-        const Vector &end2Disp = theNodes[1]->getDisp();
-        const Vector &end3Disp = theNodes[2]->getDisp();
-
-        for (int i = 0; i < 2; i++) {
-			coords(0,i) = end1Crd(i) + end1Disp(i)*fact;
-	        coords(1,i) = end2Crd(i) + end2Disp(i)*fact;    
-	        coords(2,i) = end3Crd(i) + end3Disp(i)*fact;    
-        }
-    } else {
-		int mode = displayMode * -1;
-        const Matrix &eigen1 = theNodes[0]->getEigenvectors();
-        const Matrix &eigen2 = theNodes[1]->getEigenvectors();
-        const Matrix &eigen3 = theNodes[2]->getEigenvectors();
-        if (eigen1.noCols() >= mode) {
-			for (int i = 0; i < 2; i++) {
-				coords(0,i) = end1Crd(i) + eigen1(i,mode-1)*fact;
-	            coords(1,i) = end2Crd(i) + eigen2(i,mode-1)*fact;
-	            coords(2,i) = end3Crd(i) + eigen3(i,mode-1)*fact;
-	        }    
-       } else {
-		   for (int i = 0; i < 2; i++) {
-			   coords(0,i) = end1Crd(i);
-	           coords(1,i) = end2Crd(i);
-	           coords(2,i) = end3Crd(i);
-	       }    
-       }
-    }
-    
-    int error = 0;
-
-    // finally we draw the element using drawPolygon
-    error += theViewer.drawPolygon (coords, values);
-
-    return error;
+	// draw the polygon
+	return theViewer.drawPolygon(coords, values, this->getTag());
 }
 
 Response*
