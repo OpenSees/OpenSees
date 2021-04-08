d0d286d893d4511a3f9a16e8bf9fb21fe986580b
Zarak Kasi <kasiz@xena.engr.oregonstate.edu>
2021-04-08 14:35:10 -0700

    Added setNodePressure Command

--- SRC/interpreter/PythonWrapper.cpp
+++ SRC/interpreter/PythonWrapper.cpp
@@ -1163,6 +1163,18 @@ static PyObject *Py_ops_nodePressure(PyObject *self, PyObject *args)
     return wrapper->getResults();
 }
 
+static PyObject *Py_ops_setNodePressure(PyObject *self, PyObject *args)
+{
+    wrapper->resetCommandLine(PyTuple_Size(args), 1, args);
+
+    if (OPS_setNodePressure() < 0) {
+	opserr<<(void*)0;
+	return NULL;
+    }
+
+    return wrapper->getResults();
+}
+
 static PyObject *Py_ops_nodeBounds(PyObject *self, PyObject *args)
 {
     wrapper->resetCommandLine(PyTuple_Size(args), 1, args);
@@ -2321,6 +2333,7 @@ PythonWrapper::addOpenSeesCommands()
     addCommand("nodeDOFs", &Py_ops_nodeDOFs);
     addCommand("nodeMass", &Py_ops_nodeMass);
     addCommand("nodePressure", &Py_ops_nodePressure);
+    addCommand("setNodePressure", &Py_ops_setNodePressure);
     addCommand("nodeBounds", &Py_ops_nodeBounds);
     addCommand("start", &Py_ops_startTimer);
     addCommand("stop", &Py_ops_stopTimer);
