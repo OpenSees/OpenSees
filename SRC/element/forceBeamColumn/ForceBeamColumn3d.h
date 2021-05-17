3758ce5266539bbf11146052510a455e3add1c0a
Michael H. Scott <michael.scott@oregonstate.edu>
2021-05-17 09:36:37 -0700

    Update ForceBeamColumn3d.h
    More _DLL stuff that will not be maintained

--- SRC/element/forceBeamColumn/ForceBeamColumn3d.h
+++ SRC/element/forceBeamColumn/ForceBeamColumn3d.h
@@ -131,12 +131,7 @@ class ForceBeamColumn3d: public Element
   int getResponseSensitivity(int responseID, int gradNumber,
 			     Information &eleInformation);
   // AddingSensitivity:END ///////////////////////////////////////////
-#if _DLL
-  BeamIntegration* beamIntegr;
-  int numSections;
-  SectionForceDeformation** sections;          // array of pointers to sections
-  CrdTransf* crdTransf;        // pointer to coordinate tranformation object 
-#endif
+
  protected:
   void setSectionPointers(int numSections, SectionForceDeformation **secPtrs);
   int getInitialFlexibility(Matrix &fe);
@@ -157,12 +152,11 @@ class ForceBeamColumn3d: public Element
   // internal data
   ID     connectedExternalNodes; // tags of the end nodes
 
-#if !_DLL
   BeamIntegration* beamIntegr;
   int numSections;
   SectionForceDeformation** sections;          // array of pointers to sections
   CrdTransf* crdTransf;        // pointer to coordinate tranformation object 
-#endif
+
   // (performs the transformation between the global and basic system)
   double rho;                    // mass density per unit length
   int    maxIters;               // maximum number of local iterations
