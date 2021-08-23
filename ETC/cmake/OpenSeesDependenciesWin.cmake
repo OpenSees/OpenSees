11db95a18c3fc3d5b4c5b1c4430bdf0202e1bca3
claudioperez <50180406+claudioperez@users.noreply.github.com>
2021-08-23 08:20:18 -0700

    cmp - windows dependencies

--- ETC/cmake/OpenSeesDependenciesWin.cmake
+++ ETC/cmake/OpenSeesDependenciesWin.cmake
@@ -30,15 +30,17 @@
 # - FIND:   Use CMake to find library, fail if not found
 # - SEARCH: Try finding library with CMake, build OpenSees
 #           Version if not found.
-# - PATHS:  Provide specific paths for library.
+# - BUNDLED:  Provide specific paths for library.
 #
 #==============================================================================
 set(CONDA_DIR "C:/Users/claud/miniconda3")
 set(CONDA_ENV "C:/Users/claud/miniconda3/envs/sim")
+set(BUNDLE_LIBS "${PROJECT_SOURCE_DIR}/Win64/lib/debug/")
 
 opensees_load(TCL                                          #FIND
-	LIBRARY ${CONDA_DIR}/Library/lib/tcl86t.lib
-	INCLUDE ${CONDA_DIR}/Library/include 
+    LIBRARY ${CONDA_DIR}/Library/lib/tcl86t.lib
+    #LIBRARY "${BUNDLE_LIBS}/tcl.lib"
+    INCLUDE ${CONDA_DIR}/Library/include 
 )
 
 set(TCL_INCLUDE_PATH ${TCL_INCLUDE_DIRS})
@@ -46,22 +48,20 @@ set(TCL_LIBRARY ${TCL_LIBRARIES})
 
 message("TCL: ${TCL_INCLUDE_PATH}")
 
-opensees_load(BLAS                                         #FIND
-	LIBRARY ${CONDA_ENV}/Library/lib/blas.lib
+opensees_load(BLAS
+    LIBRARY "${BUNDLE_LIBS}/blas.lib"
 )
 
-opensees_load(CBLAS                                         #FIND
-	LIBRARY ${CONDA_ENV}/Library/lib/cblas.lib
-	INCLUDE ${CONDA_ENV}/Library/include/
+opensees_load(CBLAS
+    LIBRARY "${BUNDLE_LIBS}/cblas.lib"
 )
 
-opensees_load(LAPACK                                       #FIND
-	LIBRARY ${CONDA_ENV}/Library/lib/lapack.lib
-	INCLUDE ${CONDA_ENV}/Library/include/
+opensees_load(LAPACK
+    LIBRARY "${BUNDLE_LIBS}/lapack.lib"
 )
 
 set(ENV{SUPERLU_DIR})
-opensees_load(SUPERLU                                       #SEARCH
+opensees_load(SUPERLU
     BUNDLED ${OPS_BUNDLED_DIR}/SuperLU_5.1.1/
 )
 
@@ -74,9 +74,10 @@ opensees_load(METIS                                        SEARCH)
 opensees_load(HDF5                                           FIND)
 
 opensees_load(MySQL                                          #FIND
-	LIBRARY ${CONDA_ENV}/Library/lib/libmysql.lib
-	INCLUDE ${CONDA_ENV}/Library/include/mysql
+    LIBRARY ${CONDA_ENV}/Library/lib/libmysql.lib
+    INCLUDE ${CONDA_ENV}/Library/include/mysql
 )
 
 set(MYSQL_INCLUDE_DIR "${CONDA_ENV}/Library/include/mysql/")
+
 
