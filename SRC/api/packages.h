3d2e225fc955114ad3c9141e563aa87ab791664e
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2021-10-30 19:26:56 -0700

    cmp - add include guard to packages.h

--- SRC/api/packages.h
+++ SRC/api/packages.h
@@ -1,4 +1,5 @@
-
+#ifndef OPS_PACKAGES_H
+#define OPS_PACKAGES_H
 /*                                                                        
 ** $Revision: 1.1 $
 ** $Date: 2008-12-01 23:33:16 $
@@ -9,3 +10,4 @@
 
 
 int getLibraryFunction(const char *libName, const char *functName, void **libHandle, void **funcHandle);
+#endif // OPS_PACKAGES_H
