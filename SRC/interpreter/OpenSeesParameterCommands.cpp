a0b8018d9a50e386f1da36f0503d6188e1089b3a
millen1m <maxim.millen@gmail.com>
2022-02-21 10:15:13 +1300

    cmake now builds on macos

--- SRC/interpreter/OpenSeesParameterCommands.cpp
+++ SRC/interpreter/OpenSeesParameterCommands.cpp
@@ -48,7 +48,7 @@ ReliabilityDomain *OPS_GetReliabilityDomain();
 
 int OPS_Parameter() {
   Domain *theDomain = OPS_GetDomain();
-  ReliabilityDomain *theReliabilityDomain = OPS_GetReliabilityDomain();
+//  ReliabilityDomain *theReliabilityDomain = OPS_GetReliabilityDomain();
   if (theDomain == 0) return -1;
 
   // check at least two arguments so don't segment fault on strcmp
