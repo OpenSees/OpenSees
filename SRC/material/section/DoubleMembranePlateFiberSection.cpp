a49e103e1498743d89db8ad3e9cc5a02dd4ead2f
Massimo Petracca <mazuma88@hotmail.com>
2022-06-01 12:22:13 +0200

    add setParam to shell sections with fibers

--- SRC/material/section/DoubleMembranePlateFiberSection.cpp
+++ SRC/material/section/DoubleMembranePlateFiberSection.cpp
@@ -883,3 +883,49 @@ DoubleMembranePlateFiberSection::getResponse(int responseID, Information &sectIn
   // this function, but keeping it here just for clarity
   return SectionForceDeformation::getResponse(responseID, sectInfo);
 }
+
+int DoubleMembranePlateFiberSection::setParameter(const char** argv, int argc, Parameter& param)
+{
+    // if the user explicitly wants to update a material in this section...
+    if (argc > 1) {
+        // case 1: fiber value (all fibers)
+        // case 2: fiber id value (one specific fiber)
+        if (strcmp(argv[0], "fiber") == 0 || strcmp(argv[0], "Fiber") == 0) {
+            // test case 2 (one specific fiber) ...
+            if (argc > 2) {
+                int pointNum = atoi(argv[1]);
+                if (pointNum > 0 && pointNum <= 2*numFibers) {
+                    return theFibers[pointNum - 1]->setParameter(&argv[2], argc - 2, param);
+                }
+            }
+            // ... otherwise case 1 (all fibers), if the argv[1] is not a valid id
+            int mixed_result = -1;
+            for (int i = 0; i < 2*numFibers; ++i) {
+                if (theFibers[i]->setParameter(&argv[1], argc - 1, param) == 0)
+                    mixed_result = 0; // if at least one fiber handles the param, make it successful
+            }
+            return mixed_result;
+        }
+    }
+    // if we are here, the first keyword is not "fiber", so we can check for parameters
+    // specific to this section (if any) or forward the request to all fibers.
+    if (argc > 0) {
+        // we don't have parameters for this section, so we directly forward it to all fibers.
+        // placeholder for future implementations: if we will have parameters for this class, check them here
+        // before forwarding to all fibers
+        int mixed_result = -1;
+        for (int i = 0; i < 2*numFibers; ++i) {
+            if (theFibers[i]->setParameter(argv, argc, param) == 0)
+                mixed_result = 0; // if at least one fiber handles the param, make it successful
+        }
+        return mixed_result;
+    }
+    // fallback to base class implementation
+    return SectionForceDeformation::setParameter(argv, argc, param);
+}
+
+int DoubleMembranePlateFiberSection::updateParameter(int parameterID, Information& info)
+{
+    // placeholder for future implementations: if we will have parameters for this class, update them here
+    return SectionForceDeformation::updateParameter(parameterID, info);
+}
