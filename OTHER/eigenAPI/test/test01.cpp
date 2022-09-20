61a1367e1e6717deceff64b196d58f93fa3e453c
Jose A. Abell <jaabell@uandes.cl>
2022-09-20 18:19:06 -0300

    Added headers to the test files

--- OTHER/eigenAPI/test/test01.cpp
+++ OTHER/eigenAPI/test/test01.cpp
@@ -1,3 +1,30 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 1999, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+
+// Jose Abell (UANDES, github.com/jaabell)
+// Massimo Petracca - ASDEA Software, Italy (2022)
+//
+// Basic test of the EigenAPI
+//
+
+
 #include <iostream>
 #include "../EigenAPI.h"
  
@@ -5,8 +32,7 @@ using Eigen::MatrixXd;
 using Eigen::Matrix2d;
 using Eigen::Matrix3d;
 using Eigen::Matrix4d;
-// using Eigen::Matrix5d;
-// using Eigen::Matrix6d
+
  
 int main()
 {
