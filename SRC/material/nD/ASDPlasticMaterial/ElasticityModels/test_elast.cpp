e963489b9f7f459a4f7ac367fff1916592de8c5b
Jose A. Abell <jaabell@uandes.cl>
2022-10-24 14:47:33 -0300

    adds linear elasticity

--- SRC/material/nD/ASDPlasticMaterial/ElasticityModels/test_elast.cpp
+++ SRC/material/nD/ASDPlasticMaterial/ElasticityModels/test_elast.cpp
@@ -1,34 +1,26 @@
 #include "LinearIsotropic3D_EL.h"
-#include "../../../ltensor/LTensor.h"
 
 
 int main(void)
 {
 
-    Index<'i'> i;
-    Index<'j'> j;
-    Index<'k'> k;
-    Index<'l'> l;
 
     double E = 1000;
     double nu = 0.25;
 
     LinearIsotropic3D_EL elasticity(E, nu);
 
-    VoigtVector eps(3, 3, 0.0);
-    VoigtVector sig(3, 3, 0.0);
-    VoigtMatrix Et(3, 3, 3, 3, 0.0);
+    VoigtVector depsilon = {1.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    VoigtVector sigma = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
-    Et = elasticity(sig);
+    VoigtMatrix Et = elasticity(sigma);
 
-    eps(0, 2) = 1;
-    eps(2, 0) = 1;
+    VoigtVector dsigma = Et * depsilon;
 
-    sig(i, j) = Et(i, j, k, l) * eps(k, l);
-
-    cout << "eps = " << eps << endl;
-    cout << "sig = " << sig << endl;
-    // cout << "Et = " << Et << endl;
+    cout << "\ndepsilon = \n" << depsilon.transpose() << endl;
+    cout << "\nsigma = \n" << sigma.transpose() << endl;
+    cout << "\ndsigma = \n" << dsigma.transpose() << endl;
+    cout << "\nEt = \n" << Et.transpose() << endl;
 
     return 0;
 }
