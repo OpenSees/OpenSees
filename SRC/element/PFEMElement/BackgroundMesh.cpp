c7b63cef43c3bd62bd218aadb1aff040e16db4d3
Minjie Zhu <zhum@oregonstate.edu>
2022-10-24 14:26:16 -0700

    PVD type for PFEMContact3D

--- SRC/element/PFEMElement/BackgroundMesh.cpp
+++ SRC/element/PFEMElement/BackgroundMesh.cpp
@@ -3681,9 +3681,9 @@ int BackgroundMesh::createContact3D(const VInt& ndtags,
     }
 
     // faces in three directions
-    int faceNodes[3][8] = {{1, 3, 5, 7, 0, 2, 4, 6},
-                           {2, 3, 6, 7, 0, 1, 4, 5},
-                           {4, 5, 6, 7, 0, 1, 2, 3}};
+    int faceNodes[3][8] = {{1, 3, 7, 5, 0, 2, 6, 4},
+                           {2, 3, 7, 6, 0, 1, 5, 4},
+                           {4, 5, 7, 6, 0, 1, 3, 2}};
 
     // the number of different group in opposite faces
     double maxDiffSid = 0;
