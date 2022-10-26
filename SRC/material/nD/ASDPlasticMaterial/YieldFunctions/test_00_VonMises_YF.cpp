dea293ae329b60e38a6d40bdabcad3e4bc82e863
Jose A. Abell <jaabell@uandes.cl>
2022-10-26 11:06:45 -0300

    Better testing of VonMises

--- SRC/material/nD/ASDPlasticMaterial/YieldFunctions/test_00_VonMises_YF.cpp
+++ SRC/material/nD/ASDPlasticMaterial/YieldFunctions/test_00_VonMises_YF.cpp
@@ -8,8 +8,8 @@ int main(void)
 {
 
     double H_scalar = 1;
-    double initial_scalar = 1.0;
-    LinearHardeningScalar_EV kH(H_scalar, initial_scalar);
+    double k = 1.0;
+    LinearHardeningScalar_EV kH(H_scalar, k);
 
     double H_tensor = 0;
     VoigtVector alpha01(0,0,0,0,0,0);
@@ -26,12 +26,22 @@ int main(void)
     VoigtVector depsilon;
     VoigtVector m;
     // VoigtVector sigma = {1., 1., 1., 1., 1., 1.0};
+
+    double p = 10;
+    double qmax = 2;
+    double dq = 0.25;
+    for (double q = 0; q < qmax; q += dq)
+    {
+        double sigma_xx = p;
+        double sigma_yy = p;
+        double sigma_zz = p + q;
+        double sigma_xy = 0;
+        double sigma_yz = 0;
+        double sigma_xz = 0;
+        stresses.push_back({sigma_xx, sigma_yy, sigma_zz, sigma_xy, sigma_yz, sigma_xz});
+    }
     
-    stresses.push_back({0., 0., 0., 0., 0., 0.});
-    stresses.push_back({1., 1., 1., 0., 0., 0.});
-    stresses.push_back({0., 0., 0., 1., 1., 1.});
-    stresses.push_back({2., 1., 1., 0., 0., 0.});
-    stresses.push_back({1., 1., 1., 1., 1., 1.});
+
 
     int index = 0;
     for (auto i = stresses.begin(); i != stresses.end(); ++i, index++)
@@ -47,7 +57,7 @@ int main(void)
         auto yf1_der = yf1.df_dsigma_ij(sigma);
         cout << "   yf1_der = " << yf1_der.transpose() << endl;
 
-        auto xi_star_h_star = yf1.xi_star_h_star(depsilon, m, sigma);
+        auto xi_star_h_star = yf1.xi_star_h_star(depsilon, yf1_der, sigma);
         cout << "   xi_star_h_star = " << xi_star_h_star << endl;
 
 
