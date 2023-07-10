339ede70f48ae571579929e3e7f62a00db045d87
Codi McKee <cmckee@tamu.edu>
2023-07-10 16:47:58 -0500

    Correct getStartTime() for Pulse Rect Triangle and Trig series

--- SRC/domain/pattern/PulseSeries.h
+++ SRC/domain/pattern/PulseSeries.h
@@ -65,7 +65,7 @@ public:
     double getDuration () {return tFinish-tStart;}
     double getPeakFactor () {return cFactor;}
     double getTimeIncr (double pseudoTime) {return tFinish-tStart;}
-    double getStartTime() { return 0.0; } // dummy function
+    double getStartTime() { return tStart; } 
     // methods for output    
     int sendSelf(int commitTag, Channel &theChannel);
     int recvSelf(int commitTag, Channel &theChannel, 
