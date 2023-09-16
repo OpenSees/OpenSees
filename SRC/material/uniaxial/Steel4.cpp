d5a4a460b6f5f96fa6838a7d20a6487c2fefea04
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2023-09-16 06:27:10 +0300

    Reorder send operations in `sendSelf` (2)
    bug fix

--- SRC/material/uniaxial/Steel4.cpp
+++ SRC/material/uniaxial/Steel4.cpp
@@ -923,14 +923,14 @@ int Steel4::sendSelf(int commitTag, Channel &theChannel) {
   }
   
   // Send the size vector
-  res = theChannel.sendVector(this->getDbTag(), commitTag, dataCycl);
+  int res = theChannel.sendVector(this->getDbTag(), commitTag, dataCycl);
   if (res < 0) {
     opserr << "Pinching4Material::sendSelf() - failed to send data\n";
     return res;
   }
 
   // Send the data vector
-  int res = theChannel.sendVector(this->getDbTag(), commitTag, data);
+  res = theChannel.sendVector(this->getDbTag(), commitTag, data);
   if (res < 0) {
     opserr << "Pinching4Material::sendSelf() - failed to send data\n";
     return res;
