7ae5888373157f78a2b1995da26bd60d5e8a1a28
Michael H. Scott <michael.scott@oregonstate.edu>
2024-04-09 16:32:29 -0700

    Changing size of buffer

--- SRC/element/truss/Truss.cpp
+++ SRC/element/truss/Truss.cpp
@@ -1174,7 +1174,7 @@ Truss::setResponse(const char **argv, int argc, OPS_Stream &output)
 
     if ((strcmp(argv[0],"force") == 0) || (strcmp(argv[0],"forces") == 0) 
         || (strcmp(argv[0],"globalForce") == 0) || (strcmp(argv[0],"globalForces") == 0)){
-            char outputData[10];
+            char outputData[40];
             int numDOFperNode = numDOF/2;
             for (int i=0; i<numDOFperNode; i++) {
                 sprintf(outputData,"P1_%d", i+1);
