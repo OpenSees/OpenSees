6590dc7ef1aabf980c148ff028640ba3908646d9
Telemachos Panagiotakos <tpanagiotakos@dencopc.gr>
2024-05-27 12:58:08 +0300

    test

--- SRC/domain/load/Beam3dPartialUniformLoad.cpp
+++ SRC/domain/load/Beam3dPartialUniformLoad.cpp
@@ -31,7 +31,7 @@
 #include <Parameter.h>
 
 Vector Beam3dPartialUniformLoad::data(8);
-//TPanagiotakos
+//TPanagiotakos...
 
 Beam3dPartialUniformLoad::Beam3dPartialUniformLoad(int tag, double wya, double wza, double waa,
 						   double aL, double bL, double wyb, double wzb, double wab, int theElementTag)
