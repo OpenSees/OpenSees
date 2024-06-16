3d09ab92383efa7fc8236b6a4c0a3ab9d9130b11
Minjie Zhu <zhum@oregonstate.edu>
2024-06-16 08:18:36 -0700

    3.6.0.2

--- SRC/interpreter/PythonWrapper.cpp
+++ SRC/interpreter/PythonWrapper.cpp
@@ -43,7 +43,7 @@ UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #include "OpenSeesCommands.h"
 #include <OPS_Globals.h>
 
-#define OPS_PYVERSION "3.6.0.1"
+#define OPS_PYVERSION "3.6.0.2"
 
 static PythonWrapper* wrapper = 0;
 
