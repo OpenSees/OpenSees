ea7f030fd7639f35c3d0397ebfe694239f9417bb
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-08-29 18:03:15 -0700

    cmp - update integrator commands

--- SRC/runtime/commands/analysis/integrator.cpp
+++ SRC/runtime/commands/analysis/integrator.cpp
@@ -21,313 +21,118 @@
 #include <DisplacementControl.h>
 
 #include <Newmark.h>
-#include <TRBDF2.h>
-#include <TRBDF3.h>
-#include <Houbolt.h>
-#include <ParkLMS3.h>
 #include <BackwardEuler.h>
-
-class G3_Runtime;
 
 extern "C" int OPS_ResetInputNoBuilder(ClientData clientData,
                                        Tcl_Interp *interp, int cArg, int mArg,
                                        TCL_Char ** const argv, Domain *domain);
 
 
-StaticIntegrator*
-G3Parse_newHSIntegrator(ClientData, Tcl_Interp*, int, TCL_Char ** const);
-StaticIntegrator*
-G3Parse_newLoadControl(ClientData, Tcl_Interp*, int argc, TCL_Char *argv[]);
-StaticIntegrator*
-G3Parse_newEQPathIntegrator(ClientData, Tcl_Interp*, int argc, TCL_Char ** const);
-StaticIntegrator*
-G3Parse_newArcLengthIntegrator(ClientData, Tcl_Interp*, int argc, TCL_Char ** const);
-StaticIntegrator*
-G3Parse_newMinUnbalDispNormIntegrator(ClientData, Tcl_Interp*, int, TCL_Char ** const);
-StaticIntegrator*
-G3Parse_newDisplacementControlIntegrator(ClientData, Tcl_Interp*, int, TCL_Char** const);
-StaticIntegrator*
-G3Parse_newStaticIntegrator(ClientData, Tcl_Interp*, int, TCL_Char ** const);
-
-TransientIntegrator*
-G3Parse_newNewmark1Integrator(ClientData, Tcl_Interp*, int, TCL_Char ** const);
-TransientIntegrator*
-G3Parse_newNewmarkIntegrator(ClientData, Tcl_Interp*, int, TCL_Char** const);
-TransientIntegrator*
-G3Parse_newTransientIntegrator(ClientData, Tcl_Interp*, int, TCL_Char ** const);
+Tcl_CmdProc TclCommand_newStaticIntegrator;
+Tcl_CmdProc TclCommand_newTransientIntegrator;
 
 //
-// command invoked to select and construct an integrator
+// Command invoked to select and construct an integrator
 //
 int
 specifyIntegrator(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char ** const argv)
 {
   assert(clientData != nullptr);
   OPS_ResetInputNoBuilder(clientData, interp, 2, argc, argv, nullptr);
-  BasicAnalysisBuilder *builder = (BasicAnalysisBuilder*)clientData;
 
-  // make sure at least one other argument to select integrator
+  // Make sure at least one other argument to select integrator
   if (argc < 2) {
     opserr << "WARNING need to specify an Integrator type \n";
     return TCL_ERROR;
   }
 
-  StaticIntegrator* static_integrator = 
-    G3Parse_newStaticIntegrator(clientData, interp, argc, argv);
+  if (TclCommand_newStaticIntegrator(clientData, interp, argc, argv) == TCL_OK)
+    return TCL_OK;
 
-  TransientIntegrator* transient_integrator = 
-    G3Parse_newTransientIntegrator(clientData, interp, argc, argv);
+  else if (TclCommand_newTransientIntegrator(clientData, interp, argc, argv) == TCL_OK)
+    return TCL_OK;
 
-  if (static_integrator != nullptr) {
-    opsdbg << G3_DEBUG_PROMPT << "Set integrator to \n";
-    static_integrator->Print(opsdbg);
-    builder->set(*static_integrator);
-
-  } else if (transient_integrator != nullptr) {
-    opsdbg << G3_DEBUG_PROMPT << "Set integrator to \n";
-    transient_integrator->Print(opsdbg);
-    builder->set(*transient_integrator);
-  }
   else
     return TCL_ERROR;
 
-  return TCL_OK;
 }
 
-StaticIntegrator*
-G3Parse_newStaticIntegrator(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char ** const argv)
+int
+TclCommand_newStaticIntegrator(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char ** const argv)
 {
+  BasicAnalysisBuilder *builder = (BasicAnalysisBuilder*)clientData;
 
-  StaticIntegrator* the_static_integrator = nullptr;
+  auto tcl_cmd = StaticIntegratorLibrary.find(std::string(argv[1]));
+  if (tcl_cmd != StaticIntegratorLibrary.end())
+    return (*tcl_cmd->second)(clientData, interp, argc, &argv[0]);
 
-  // check argv[1] for type of integrator and create the object
+  //
+  StaticIntegrator* theStaticIntegrator = nullptr;
 
-  if (strcmp(argv[1], "LoadControl") == 0) {
-    the_static_integrator = G3Parse_newLoadControl(clientData, interp, argc, argv);
-  } 
+  // Check argv[1] for type of integrator and create the object
 
-  else if (strcmp(argv[1], "EQPath") == 0) {
-    the_static_integrator = G3Parse_newEQPathIntegrator(clientData, interp, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "MinUnbalDispNorm") == 0) {
-    the_static_integrator = G3Parse_newMinUnbalDispNormIntegrator(clientData, interp, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "DisplacementControl") == 0) {
-    the_static_integrator = G3Parse_newDisplacementControlIntegrator(clientData, interp, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "ArcLength") == 0) {
-    the_static_integrator = G3Parse_newArcLengthIntegrator(clientData, interp, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "ArcLength1") == 0) {
+  if (strcmp(argv[1], "ArcLength1") == 0) {
     double arcLength;
     double alpha;
     if (argc != 4) {
       opserr << "WARNING integrator ArcLength1 arcLength alpha \n";
-      return nullptr;
+      return TCL_ERROR;
     }
     if (Tcl_GetDouble(interp, argv[2], &arcLength) != TCL_OK)
-      return nullptr;
+      return TCL_ERROR;
     if (Tcl_GetDouble(interp, argv[3], &alpha) != TCL_OK)
-      return nullptr;
-    the_static_integrator = new ArcLength1(arcLength, alpha);
+      return TCL_ERROR;
+    theStaticIntegrator = new ArcLength1(arcLength, alpha);
   }
 
-
-//else if (strcmp(argv[1], "HSConstraint") == 0) {
-//  the_static_integrator = G3Parse_newHSIntegrator(clientData, interp, argc, argv);
-//}
-
-  return the_static_integrator;
+  //
+  // Done parsing; set the integrator
+  //
+  if (theStaticIntegrator != nullptr) {
+    opsdbg << G3_DEBUG_PROMPT << "Set integrator to \n";
+    theStaticIntegrator->Print(opsdbg);
+    builder->set(*theStaticIntegrator);
+    return TCL_OK;
+  } else
+    return TCL_ERROR;
 }
 
-TransientIntegrator*
-G3Parse_newTransientIntegrator(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char ** const argv)
+int
+TclCommand_newTransientIntegrator(ClientData clientData, Tcl_Interp *interp, int argc, TCL_Char ** const argv)
 {
-  G3_Runtime* rt = G3_getRuntime(interp);
+  BasicAnalysisBuilder *builder = (BasicAnalysisBuilder*)clientData;
+
+  auto tcl_cmd = TransientIntegratorLibrary.find(std::string(argv[1]));
+  if (tcl_cmd != TransientIntegratorLibrary.end())
+    return (*tcl_cmd->second)(clientData, interp, argc, &argv[0]);
+
+
   TransientIntegrator * theTransientIntegrator = nullptr;
 
-  if ((strcmp(argv[1], "TRBDF2") == 0) ||
-      (strcmp(argv[1], "Bathe") == 0)) {
-    theTransientIntegrator = new TRBDF2();
-  }
 
-  else if ((strcmp(argv[1], "TRBDF3") == 0) ||
-           (strcmp(argv[1], "Bathe3") == 0)) {
-    theTransientIntegrator = new TRBDF3();
-  }
-
-  else if (strcmp(argv[1], "Houbolt") == 0) {
-    theTransientIntegrator = new Houbolt();
-  }
-
-  /*else if (strcmp(argv[1],"ParkLMS3") == 0) {
-      theTransientIntegrator = new ParkLMS3();
-  }*/
-
-  else if (strcmp(argv[1], "BackwardEuler") == 0) {
+  if (strcmp(argv[1], "BackwardEuler") == 0) {
     int optn = 0;
     if (argc == 3) {
       if (Tcl_GetInt(interp, argv[2], &optn) != TCL_OK) {
         opserr << "WARNING integrator BackwardEuler <option> - undefined "
                   "option specified\n";
-        return nullptr;
+        return TCL_ERROR;
       }
     }
     theTransientIntegrator = new BackwardEuler(optn);
   }
 
-  else if (strcmp(argv[1], "Newmark") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)G3Parse_newNewmarkIntegrator(clientData, interp, argc, argv);
+  //
+  //
+  //
+  if (theTransientIntegrator == nullptr) {
+    return TCL_ERROR;
   }
 
-  else if (strcmp(argv[1], "GimmeMCK") == 0 || strcmp(argv[1], "MCK") == 0 || strcmp(argv[1], "ZZTop") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_GimmeMCK(rt, argc, argv);
-  } 
-  else if (strcmp(argv[1], "NewmarkExplicit") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_NewmarkExplicit(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "NewmarkHSIncrReduct") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_NewmarkHSIncrReduct(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "NewmarkHSIncrLimit") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_NewmarkHSIncrLimit(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "NewmarkHSFixedNumIter") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_NewmarkHSFixedNumIter(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHT") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHT(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHT_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHT_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTGeneralized") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTGeneralized(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTGeneralized_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTGeneralized_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTExplicit") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTExplicit(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTExplicit_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTExplicit_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTGeneralizedExplicit") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTGeneralizedExplicit(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTGeneralizedExplicit_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTGeneralizedExplicit_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTHSIncrLimit") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTHSIncrLimit(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTHSIncrLimit_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTHSIncrLimit_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTHSIncrReduct") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTHSIncrReduct(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTHSIncrReduct_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTHSIncrReduct_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTHSFixedNumIter") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTHSFixedNumIter(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "HHTHSFixedNumIter_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_HHTHSFixedNumIter_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "GeneralizedAlpha") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_GeneralizedAlpha(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "KRAlphaExplicit") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_KRAlphaExplicit(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "KRAlphaExplicit_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_KRAlphaExplicit_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "AlphaOS") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_AlphaOS(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "AlphaOS_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_AlphaOS_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "AlphaOSGeneralized") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_AlphaOSGeneralized(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "AlphaOSGeneralized_TP") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_AlphaOSGeneralized_TP(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "Collocation") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_Collocation(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "CollocationHSIncrReduct") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_CollocationHSIncrReduct(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "CollocationHSIncrLimit") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_CollocationHSIncrLimit(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "CollocationHSFixedNumIter") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_CollocationHSFixedNumIter(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "Newmark1") == 0) {
-    theTransientIntegrator = G3Parse_newNewmark1Integrator(clientData, interp, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "WilsonTheta") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_WilsonTheta(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "ExplicitDifference") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_ExplicitDifference(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "CentralDifference") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_CentralDifference(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "CentralDifferenceAlternative") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_CentralDifferenceAlternative(rt, argc, argv);
-  }
-
-  else if (strcmp(argv[1], "CentralDifferenceNoDamping") == 0) {
-    theTransientIntegrator = (TransientIntegrator *)OPS_CentralDifferenceNoDamping(rt, argc, argv);
-  }
-
-  return theTransientIntegrator;
+  opsdbg << G3_DEBUG_PROMPT << "Set integrator to \n";
+  theTransientIntegrator->Print(opsdbg);
+  builder->set(*theTransientIntegrator);
+  return TCL_OK;
 }
 
 #include <HSConstraint.h>
@@ -590,7 +395,7 @@ G3Parse_newDisplacementControlIntegrator(ClientData clientData, Tcl_Interp *inte
     }
 
 #ifdef _PARALLEL_PROCESSING
-    the_static_integrator = new DistributedDisplacementControl(
+    theStaticIntegrator = new DistributedDisplacementControl(
         node, dof - 1, increment, numIter, minIncr, maxIncr);
 #else
     Node *theNode = domain->getNode(node);
@@ -679,6 +484,6 @@ G3Parse_newStagedLoadControlIntegrator(ClientData clientData, Tcl_Interp *interp
 
     theDDC->setProcessID(OPS_rank);
     theDDC->setChannels(numChannels, theChannels);
-    the_static_integrator = theDDC;
+    theStaticIntegrator = theDDC;
   }
 #endif
