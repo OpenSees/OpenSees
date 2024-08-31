90736ce787d8f1a011003aa77c8f49e1ec35f71a
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-08-31 13:35:31 -0700

    cmp - update SRC/runtime/runtime/SectionBuilder/patch/Patch.h

--- SRC/runtime/runtime/SectionBuilder/patch/Patch.h
+++ SRC/runtime/runtime/SectionBuilder/patch/Patch.h
@@ -2,20 +2,6 @@
 **    OpenSees - Open System for Earthquake Engineering Simulation    **
 **          Pacific Earthquake Engineering Research Center            **
 **                                                                    **
-**                                                                    **
-** (C) Copyright 1999, The Regents of the University of California    **
-** All Rights Reserved.                                               **
-**                                                                    **
-** Commercial use of this program without express permission of the   **
-** University of California, Berkeley, is strictly prohibited.  See   **
-** file 'COPYRIGHT'  in main directory for information on usage and   **
-** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
-**                                                                    **
-** Developed by:                                                      **
-**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
-**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
-**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
-**                                                                    **
 ** ****************************************************************** */
 //
 // File: Patch.h
@@ -31,20 +17,16 @@ class OPS_Stream;
 class Patch
 {
   public:
-
-    Patch();
-    virtual ~Patch();
-    
     // edition functions
 
     virtual void setMaterialID (int materialID) = 0;
     
     // inquiring functions
 
-    virtual int     getMaterialID (void) const = 0; 
-    virtual int     getNumCells   (void) const = 0;
-    virtual Cell  **getCells      (void) const = 0;
-    virtual Patch  *getCopy       (void) const = 0;
+    virtual int     getMaterialID() const = 0; 
+    virtual int     getNumCells() const = 0;
+    virtual Cell  **getCells() const = 0;
+    virtual Patch  *getCopy() const = 0;
 
     virtual void Print(OPS_Stream &s, int flag =0) const =0;   
     friend OPS_Stream &operator<<(OPS_Stream &s, const Patch &patch);    
