d0c4c15def2fde45dba2399c0cdb2133e36206f7
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-08-31 14:34:04 -0700

    cmp - update runtime

--- SRC/runtime/parsing/InterpreterAPI.cpp
+++ SRC/runtime/parsing/InterpreterAPI.cpp
@@ -172,6 +172,47 @@ OPS_GetStringCopy(char **arrayData)
 }
 
 
+extern "C"
+int OPS_GetDoubleListInput(int* size, Vector* data)
+{
+    TCL_Char** strings;
+
+    if (Tcl_SplitList(theInterp, currentArgv[currentArg],
+        size, &strings) != TCL_OK) {
+        opserr << "ERROR problem splitting list " << currentArgv[currentArg] << " \n";
+        return -1;
+    }
+
+    data->resize(*size);
+    for (int i = 0; i < *size; i++) {
+        double value;
+        if (Tcl_GetDouble(theInterp, strings[i], &value) != TCL_OK) {
+            opserr << "ERROR problem reading data value " << strings[i] << " \n";
+            // free up the array of strings .. see tcl man pages as to why
+            Tcl_Free((char*)strings);
+            return -1;
+        }
+        (*data)(i) = value;
+    }
+    // free up the array of strings .. see tcl man pages as to why
+    Tcl_Free((char*)strings);
+
+    currentArg++;
+
+    return 0;
+}
+
+extern "C"
+int OPS_EvalDoubleStringExpression(const char* theExpression, double& current_val) {
+    if (Tcl_ExprDouble(theInterp, theExpression, &current_val) != TCL_OK) {
+        opserr << "OPS_EvalDoubleStringExpression::evaluateExpression -- expression \"" << theExpression;
+        opserr << "\" caused error:" << endln << Tcl_GetStringResult(theInterp) << endln;
+        return -1;
+    }
+    return 0;
+}
+
+
 extern "C" int OPS_SetDoubleListsOutput(
     std::vector<std::vector<double>>& data) {
     // a vector holds tcl objects of lists
