88b285b23801d68a5a31c2a5975d28a50e5e8283
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-08-31 13:35:32 -0700

    cmp - update SRC/runtime/runtime/SectionBuilder/reinfLayer/CircReinfLayer.h

--- SRC/runtime/runtime/SectionBuilder/reinfLayer/CircReinfLayer.h
+++ SRC/runtime/runtime/SectionBuilder/reinfLayer/CircReinfLayer.h
@@ -17,12 +17,7 @@
 **   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
 **                                                                    **
 ** ****************************************************************** */
-                                                                        
-// $Revision: 1.3 $
-// $Date: 2003-02-14 23:01:37 $
-// $Source: /usr/local/cvs/OpenSees/SRC/material/section/repres/reinfLayer/CircReinfLayer.h,v $
-                                                                        
-                                                                        
+//
 // File: CircReinfLayer.h
 // Written by Remo M. de Souza
 // December 1998
@@ -41,12 +36,12 @@ class CircReinfLayer : public ReinfLayer
   public:
 
     CircReinfLayer();
-	// Constructor for an arc
+    // Constructor for an arc
     CircReinfLayer(int materialID, int numReinfBars, double  reinfBarArea,
                    const Vector &centerPosition, double arcRadius, double
                    initialAngle, double finalAngle);
     // Constructor for full circle
-	CircReinfLayer(int materialID, int numReinfBars, double  reinfBarArea,
+    CircReinfLayer(int materialID, int numReinfBars, double  reinfBarArea,
                    const Vector &centerPosition, double radius);
 
     ~CircReinfLayer();
@@ -65,18 +60,18 @@ class CircReinfLayer : public ReinfLayer
 
     // inquiring functions
 
-    int           getNumReinfBars     (void) const;
-    int           getMaterialID       (void) const;
-    double        getReinfBarDiameter (void) const;
-    double        getReinfBarArea     (void) const;
-    ReinfBar     *getReinfBars        (void) const;
+    int           getNumReinfBars() const;
+    int           getMaterialID() const;
+    double        getReinfBarDiameter() const;
+    double        getReinfBarArea() const;
+    ReinfBar     *getReinfBars() const;
 
   
-    ReinfLayer   *getCopy             (void) const;
-    const Vector &getCenterPosition   (void) const;
-    double        getArcRadius        (void) const;    
-    double        getInitAngle        (void) const;
-    double        getFinalAngle       (void) const;  
+    ReinfLayer   *getCopy() const;
+    const Vector &getCenterPosition() const;
+    double        getArcRadius() const;    
+    double        getInitAngle() const;
+    double        getFinalAngle() const;  
     
 
     void Print(OPS_Stream &s, int flag =0) const;   
