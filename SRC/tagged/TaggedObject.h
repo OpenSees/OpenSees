b0aea0d47947a58478f46d69bdd72fb023dd6b44
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-10-14 23:23:13 -0700

    cmp - minor correction

--- SRC/tagged/TaggedObject.h
+++ SRC/tagged/TaggedObject.h
@@ -57,8 +57,8 @@ class TaggedObject
   protected:
     void setTag(int newTag);  // CAUTION: this is a dangerous method to call
                               //
-    constexpr static char[] JsonGeometryIndent = "\t\t\t";
-    constexpr static char[] JsonPropertyIndent = "\t\t\t";
+    constexpr static char JsonGeometryIndent[] = "\t\t\t";
+    constexpr static char JsonPropertyIndent[] = "\t\t\t";
     
   private:    
     int theTag;    
