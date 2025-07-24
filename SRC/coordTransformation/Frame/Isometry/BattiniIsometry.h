0014ea654868f0aba9f8fad237061d85ace9ae32
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2025-07-24 11:25:15 -0700

    update headers

--- SRC/coordTransformation/Frame/Isometry/BattiniIsometry.h
+++ SRC/coordTransformation/Frame/Isometry/BattiniIsometry.h
@@ -2,13 +2,20 @@
 //
 //                                   xara
 //                              https://xara.so
-//----------------------------------------------------------------------------//
 //
-// The following resource should be cited in derivative works:
+//===----------------------------------------------------------------------===//
 //
-// [1] Perez, C.M., and Filippou F.C.. (2024) 
-//     "On Nonlinear Geometric Transformations of Finite Elements" 
-//     Int. J. Numer. Meth. Engrg.; https://doi.org/10.1002/nme.7506
+// Copyright (c) 2025, Claudio M. Perez
+// All rights reserved.  No warranty, explicit or implicit, is provided.
+//
+// This source code is licensed under the BSD 2-Clause License.
+// See LICENSE file or https://opensource.org/licenses/BSD-2-Clause
+//
+// Please cite the following resource in any derivative works:
+//
+// Perez, C.M., and Filippou F.C.. "On Nonlinear Geometric Transformations
+//     of Finite Elements" Int. J. Numer. Meth. Engrg. 2024; 
+//     https://doi.org/10.1002/nme.7506
 //
 //===----------------------------------------------------------------------===//
 
--- SRC/runtime/commands/modeling/geomTransf.cpp
+++ SRC/runtime/commands/modeling/geomTransf.cpp
@@ -1,10 +1,38 @@
 //===----------------------------------------------------------------------===//
 //
 //                                   xara
+//                              https://xara.so
 //
 //===----------------------------------------------------------------------===//
-//                              https://xara.so
+//
+// Copyright (c) 2025, Claudio M. Perez
+// All rights reserved.  No warranty, explicit or implicit, is provided.
+//
+// This source code is licensed under the BSD 2-Clause License.
+// See LICENSE file or https://opensource.org/licenses/BSD-2-Clause
+//
 //===----------------------------------------------------------------------===//
+
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 1999, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+
 //
 // Description: Geometric transformation command
 //
