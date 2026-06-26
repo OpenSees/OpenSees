4f68abdda083c74291782e9d5ac39915d6f628c8
Javad Esmaeelpour <jvd.esm@gmail.com>
2026-06-25 21:25:23 -0400

    Remove CreepMaterial; add CreepShrinkageACI209 to runtime/Win64 and CI tests

--- SRC/classTags.h
+++ SRC/classTags.h
@@ -259,7 +259,6 @@
 #define MAT_TAG_TDConcreteMC10NL 227
 #define MAT_TAG_CoulombDamperMaterial 228
 #define MAT_TAG_FlagShapeMaterial 229
-#define MAT_TAG_CreepMaterial 230
 #define MAT_TAG_ASDConcrete1DMaterial 231
 #define MAT_TAG_PipeMaterial 232
 #define MAT_TAG_TzSandCPT 233
