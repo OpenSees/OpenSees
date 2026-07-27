1d7e31f0a127af0aa17bc80dca6ea44a2f3eddea
fmk <fmckenna@berkeley.edu>
2026-07-26 22:54:18 -0700

    fmk - updating OpenSeesAWS-Ubuntu22.04 to utilze latest gcc (current 13) and updating python to latest pulled (python3.12)

--- OpenSeesAWS-Ubuntu22.04.sh
+++ OpenSeesAWS-Ubuntu22.04.sh
@@ -10,7 +10,7 @@
 
 sudo apt-get update
 sudo apt install -y cmake
-sudo apt install -y gcc-11 g++-11 gfortran-11
+sudo apt install -y gcc g++ gfortran
 sudo apt install -y python3-pip python3-venv
 sudo apt install -y liblapack-dev
 sudo apt install -y libopenmpi-dev
@@ -59,7 +59,7 @@ cd OpenSees/
 mkdir build
 conan install conan.txt --output-folder=build --build=missing
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release  -DMUMPS_DIR=$PWD/../../mumps/build -DOPENMPI=TRUE -DSCALAPACK_LIBRARIES="/usr/lib/x86_64-linux-gnu/libmkl_blacs_openmpi_lp64.so;/usr/lib/x86_64-linux-gnu/libscalapack-openmpi.so"  -DPython_LIBRARIES=/usr/lib/x86_64-linux-gnu/libpython3.10.so -DPython_INCLUDE_DIRS=/usr/include/python3.10 -DLAPACK_LIBRARIES="-m64  -Wl,--start-group /usr/lib/x86_64-linux-gnu/libmkl_intel_ilp64.so /usr/lib/x86_64-linux-gnu/libmkl_sequential.so /usr/lib/x86_64-linux-gnu/libmkl_core.so -Wl,--end-group -lpthread"
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release  -DMUMPS_DIR=$PWD/../../mumps/build -DOPENMPI=TRUE -DSCALAPACK_LIBRARIES="/usr/lib/x86_64-linux-gnu/libmkl_blacs_openmpi_lp64.so;/usr/lib/x86_64-linux-gnu/libscalapack-openmpi.so"  -DPython_LIBRARIES=/usr/lib/x86_64-linux-gnu/libpython3.12.so -DPython_INCLUDE_DIRS=/usr/include/python3.12 -DLAPACK_LIBRARIES="-m64  -Wl,--start-group /usr/lib/x86_64-linux-gnu/libmkl_intel_ilp64.so /usr/lib/x86_64-linux-gnu/libmkl_sequential.so /usr/lib/x86_64-linux-gnu/libmkl_core.so -Wl,--end-group -lpthread"
 
 cmake --build . --config Release --target OpenSees   --parallel 4
 cmake --build . --config Release --target OpenSeesPy --parallel 4
